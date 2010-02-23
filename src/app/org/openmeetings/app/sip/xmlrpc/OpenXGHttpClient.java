d9989a0dbcc4ec71c32842ee0bec67aae1f8fbf4
seba.wagner <seba.wagner@unknown>
2010-02-23 10:39:09 +0000

    Testing
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/import/trunk/singlewebapp@1218605 13f79535-47bb-0310-9956-ffa450edef68

--- src/app/org/openmeetings/app/sip/xmlrpc/OpenXGHttpClient.java
+++ src/app/org/openmeetings/app/sip/xmlrpc/OpenXGHttpClient.java
@@ -8,9 +8,10 @@ import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpVersion;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
 import org.openmeetings.utils.crypt.MD5;
 import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
@@ -35,112 +36,113 @@ public class OpenXGHttpClient {
 			log.debug("Test Connection");
 			
 			// Get target URL
-	        String strURL = "http://85.134.48.179/manager/xml_rpc_server.php";
+	        String strURL = "http://****/manager/xml_rpc_server.php";
 	        // Get file to be posted
-//	        String stringToPost = "<?xml version=\"1.0\" ?>"+ System.getProperty("line.separator") +
-//										"<methodCall>"+ System.getProperty("line.separator") +
-//										"	<methodName>OpenSIPg.UserCreate</methodName>"+ System.getProperty("line.separator") +
-//										"	<params>"+ System.getProperty("line.separator") +
-//										"		<param>"+ System.getProperty("line.separator") +
-//										"			<value>"+ System.getProperty("line.separator") +
-//										"				<struct>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>client_id</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>user_admin</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>digest</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>23fe6626265cb55fc7b631f8c043ea1a</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>userid</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>067201101</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>domain</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>voipt3multifi</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>first_name</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>Matti</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>middle_i</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>X</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>last_name</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>Virtanen</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>password</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>password</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>community_code</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>999</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>language_code</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>fi</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>email</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>matti@suckscom</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"					<member>"+ System.getProperty("line.separator") +
-//										"						<name>adminid</name>"+ System.getProperty("line.separator") +
-//										"						<value>"+ System.getProperty("line.separator") +
-//										"							<string>matti</string>"+ System.getProperty("line.separator") +
-//										"						</value>"+ System.getProperty("line.separator") +
-//										"					</member>"+ System.getProperty("line.separator") +
-//										"				</struct>"+ System.getProperty("line.separator") +
-//										"			</value>"+ System.getProperty("line.separator") +
-//										"		</param>"+ System.getProperty("line.separator") +
-//										"	</params>"+ System.getProperty("line.separator") +
-//										"</methodCall>";
-//	        
+	        String stringToPost = "<?xml version=\"1.0\" ?>"+ System.getProperty("line.separator") +
+										"<methodCall>"+ System.getProperty("line.separator") +
+										"	<methodName>OpenSIPg.UserCreate</methodName>"+ System.getProperty("line.separator") +
+										"	<params>"+ System.getProperty("line.separator") +
+										"		<param>"+ System.getProperty("line.separator") +
+										"			<value>"+ System.getProperty("line.separator") +
+										"				<struct>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>client_id</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>user_admin</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>digest</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>23fe6626265cb55fc7b631f8c043ea1a</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>userid</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>067201101</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>domain</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>voipt3multifi</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>first_name</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>Matti</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>middle_i</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>X</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>last_name</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>Virtanen</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>password</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>password</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>community_code</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>999</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>language_code</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>fi</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>email</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>matti@suckscom</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"					<member>"+ System.getProperty("line.separator") +
+										"						<name>adminid</name>"+ System.getProperty("line.separator") +
+										"						<value>"+ System.getProperty("line.separator") +
+										"							<string>matti</string>"+ System.getProperty("line.separator") +
+										"						</value>"+ System.getProperty("line.separator") +
+										"					</member>"+ System.getProperty("line.separator") +
+										"				</struct>"+ System.getProperty("line.separator") +
+										"			</value>"+ System.getProperty("line.separator") +
+										"		</param>"+ System.getProperty("line.separator") +
+										"	</params>"+ System.getProperty("line.separator") +
+										"</methodCall>";
+	        
 	        
 	        //File f = new File("NewFileISO.xml");
 	        
 	        // Prepare HTTP post
 	        PostMethod post = new PostMethod(strURL);
+	        post.addRequestHeader("User-Agent", "OpenSIPg XML_RPC Client");
+
 	        // Request content will be retrieved directly
 	        // from the input stream
 	        //RequestEntity entity = new FileRequestEntity(f, "text/xml; charset=ISO-8859-1"); 
 	        
-	        String encodedString = this.readFile("/root/openmeetings/red5/webapps/openmeetings/WEB-INF/classes/NewFileISO.xml");
+	        //String encodedString = this.readFile("/root/openmeetings/red5/webapps/openmeetings/WEB-INF/classes/NewFileISO.xml");
 	        
-	        
-	        
-	        RequestEntity entity = new StringRequestEntity(encodedString);
+	        RequestEntity entity = new ByteArrayRequestEntity(stringToPost.getBytes(Charset.forName("ISO-8859-1")));
 	        
 //	        // Prepare HTTP post
 //	        PostMethod post = new PostMethod(strURL);
 	        
 	        post.getParams().setContentCharset("ISO-8859-1");
+	        post.getParams().setVersion(HttpVersion.HTTP_1_0);
 	        
 	        // Request content will be retrieved directly
 	        // from the input stream
