f36e576bc1762ee2148e67ecee57e2f2c450c8ad
seba.wagner <seba.wagner@unknown>
2010-03-06 10:15:25 +0000

    Update Server Libs
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/import/trunk/singlewebapp@1218692 13f79535-47bb-0310-9956-ffa450edef68

--- server/red5/red5-highperf.bat
+++ server/red5/red5-highperf.bat
@@ -8,7 +8,7 @@ REM -Xrs -Xms512M -Xmx768M -Xss128K -XX:PermSize=256M -XX:MaxPermSize=512M -XX:N
 REM Latest 06/2008
 REM -Xmx768m -Xms256 -Xmn512m -Xss128k -XX:+AggressiveOpts -XX:+AggressiveHeap -XX:+DisableExplicitGC -XX:ParallelGCThreads=4 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=31 -Djava.net.preferIPv4Stack=true -Dsun.rmi.dgc.client.gcInterval=990000 -Dsun.rmi.dgc.server.gcInterval=990000
 
-set JAVA_OPTS= -Xmx768m -Xms256 -Xmn512m -Xss256k -XX:+AggressiveOpts -XX:+AggressiveHeap -XX:+DisableExplicitGC -XX:ParallelGCThreads=4 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=16 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=31 -Djava.net.preferIPv4Stack=true -Dsun.rmi.dgc.client.gcInterval=990000 -Dsun.rmi.dgc.server.gcInterval=990000
+set JAVA_OPTS= -Xmx768m -Xms256 -Xmn512m -Xss256k -XX:+AggressiveOpts -XX:+AggressiveHeap -XX:+DisableExplicitGC -XX:ParallelGCThreads=4 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=16 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=31 -Djava.net.preferIPv4Stack=true
 
 echo Running High Performance Red5
 start cmd /c %RED5_HOME%\red5.bat >> %RED5_HOME%\log\jvm.stdout
--- server/red5/red5-highperf.sh
+++ server/red5/red5-highperf.sh
@@ -3,7 +3,7 @@
 if [ -z "$RED5_HOME" ]; then export RED5_HOME=.; fi
 
 # Previous option set
-export JAVA_OPTS="-Xrs -Xms512M -Xmx1024M -Xss128K -XX:NewSize=256m -XX:SurvivorRatio=16 -XX:MinHeapFreeRatio=20 -XX:+AggressiveHeap -XX:+ExplicitGCInvokesConcurrent -XX:+UseConcMarkSweepGC -Dsun.rmi.dgc.client.gcInterval=990000 -Dsun.rmi.dgc.server.gcInterval=990000 -Djava.net.preferIPv4Stack=true -Xverify:none"
+export JAVA_OPTS="-Xrs -Xms512M -Xmx1024M -Xss128K -XX:NewSize=256m -XX:SurvivorRatio=16 -XX:MinHeapFreeRatio=20 -XX:+AggressiveHeap -XX:+ExplicitGCInvokesConcurrent -XX:+UseConcMarkSweepGC -Djava.net.preferIPv4Stack=true -Xverify:none"
 
 # start Red5
 echo "Setting Hi Performance Options"
