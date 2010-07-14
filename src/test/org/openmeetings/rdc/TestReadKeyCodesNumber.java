f2b50547ccb925f32650e95b8d3971067cf41a09
seba.wagner <seba.wagner@unknown>
2010-07-14 18:56:43 +0000

    sample testing
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/import/trunk/singlewebapp@1218858 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/openmeetings/rdc/TestReadKeyCodesNumber.java
@@ -0,0 +1,39 @@
+package org.openmeetings.rdc;
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import org.apache.log4j.Logger;
+import junit.framework.TestCase;
+
+public class TestReadKeyCodesNumber extends TestCase {
+	
+	private static final Logger log = Logger.getLogger(TestReadKeyCodesNumber.class);
+	
+	public void testTestKeyCodesNumber() {
+		try {
+			
+			this.testKeyCodes();
+			
+		} catch (Exception er) {
+			log.debug("ERROR ", er);
+		}
+	}
+	
+	 public void testKeyCodes() {
+	    	try {
+	    		
+	    		
+	    		
+	    		while (true) {
+	    			
+	    			
+	    			
+	    		}
+	    		
+	    	} catch (Exception err) {
+	    		log.error( "[testKeyCodes]", err );
+	    	}
+	    	
+	    }
+	
+}
