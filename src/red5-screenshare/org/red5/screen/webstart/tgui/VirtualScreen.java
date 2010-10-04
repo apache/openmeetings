a22c825ac8931ad1acd08c45de67cdb06fa386f9
seba.wagner <seba.wagner@unknown>
2010-10-04 10:56:41 +0000

    Default to High
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/import/trunk/singlewebapp@1218989 13f79535-47bb-0310-9956-ffa450edef68

--- src/red5-screenshare/org/red5/screen/webstart/tgui/VirtualScreen.java
+++ src/red5-screenshare/org/red5/screen/webstart/tgui/VirtualScreen.java
@@ -234,7 +234,7 @@ public class VirtualScreen {
 		
 		JComboBox comboResize  = new JComboBox(selectResize);	
 		comboResize.setBounds(250, 330, 200, 24);
-		comboResize.setSelectedIndex(1);
+		comboResize.setSelectedIndex(0);
 		comboResize.addActionListener(new GetResizeChoice()); 
 		
 		ScreenShareRTMPT.instance.jVScreenResizeMode = comboResize;
