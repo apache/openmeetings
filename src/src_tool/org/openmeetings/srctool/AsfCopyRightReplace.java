9b3ec43fec0aad3eb3657af7c064d0d48dcc8311
Sebastian Wagner <sebawagner@apache.org>
2012-01-24 18:30:48 +0000

    License Header in Source Files, P1
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1235400 13f79535-47bb-0310-9956-ffa450edef68

--- src/src_tool/org/openmeetings/srctool/AsfCopyRightReplace.java
+++ src/src_tool/org/openmeetings/srctool/AsfCopyRightReplace.java
@@ -22,7 +22,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -64,6 +63,24 @@ public class AsfCopyRightReplace {
 			+ "  KIND, either express or implied.  See the License for the\n"
 			+ "  specific language governing permissions and limitations\n"
 			+ "  under the License.\n" + "  \n" + "-->\n";
+	
+	String asf_vm_copyright = "#\n"
+			+ "# Licensed to the Apache Software Foundation (ASF) under one\n"
+			+ "# or more contributor license agreements.  See the NOTICE file\n"
+			+ "# distributed with this work for additional information\n"
+			+ "# regarding copyright ownership.  The ASF licenses this file\n"
+			+ "# to you under the Apache License, Version 2.0 (the\n"
+			+ "# \"License\") +  you may not use this file except in compliance\n"
+			+ "# with the License.  You may obtain a copy of the License at\n"
+			+ "#\n"
+			+ "#   http://www.apache.org/licenses/LICENSE-2.0\n"
+			+ "#\n"
+			+ "# Unless required by applicable law or agreed to in writing,\n"
+			+ "# software distributed under the License is distributed on an\n"
+			+ "# \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n"
+			+ "# KIND, either express or implied.  See the License for the\n"
+			+ "# specific language governing permissions and limitations\n"
+			+ "# under the License.\n" + "#/\n";
 
 	public static void main(String... args) {
 		new AsfCopyRightReplace();
@@ -73,14 +90,16 @@ public class AsfCopyRightReplace {
 		// scanFolder(new File("./src"));
 		// scanFolder(new File("./WebContent/languages/"));
 		// scanFolder(new File("./WebContent/src/"));
-		scanFolder(new File("./WebContent/WEB-INF/"));
+		// scanFolder(new File("./WebContent/WEB-INF/"));
+		// scanFolder(new File("./WebContent/openmeetings/"));
+		scanFolder(new File("./src/templates"));
 	}
 
 	private void scanFolder(File folder) {
 
 		for (File javaFile : folder.listFiles(new FilenameFilter() {
 			public boolean accept(File b, String name) {
-				return name.endsWith(".java");
+				return name.endsWith(".java") || name.endsWith(".vm");
 			}
 		})) {
 			scanAndWriteJavaFile(javaFile);
@@ -108,7 +127,7 @@ public class AsfCopyRightReplace {
 
 	private void scanAndWriteXMLFile(File javaFile) {
 		try {
-			System.err.println(" " + javaFile.getAbsolutePath());
+			System.out.println("Processing " + javaFile.getAbsolutePath());
 
 			BufferedReader is = new BufferedReader(new InputStreamReader(
 					new FileInputStream(javaFile), "UTF-8"));
@@ -160,10 +179,10 @@ public class AsfCopyRightReplace {
 
 	private void scanAndWriteJavaFile(File javaFile) {
 		try {
-			System.err.println(" " + javaFile.getAbsolutePath());
+			System.out.println("Processing " + javaFile.getAbsolutePath());
 
 			BufferedReader is = new BufferedReader(new InputStreamReader(
-					new FileInputStream(javaFile)));
+					new FileInputStream(javaFile), "UTF-8"));
 
 			String line;
 			StringWriter strWriter = new StringWriter();
@@ -173,17 +192,22 @@ public class AsfCopyRightReplace {
 				if (i == 0) {
 					if (line.startsWith("package ")) {
 						strWriter.append(asf_copyright);
+					} else if (line.startsWith("## OpenMeetings") 
+							&& javaFile.getName().endsWith(".vm")) {
+						strWriter.append(asf_vm_copyright);
 					}
 				}
 				strWriter.append(line + "\n");
 				i++;
 			}
 			is.close();
+			
+			OutputStreamWriter out = new OutputStreamWriter(
+					new FileOutputStream(javaFile.getAbsolutePath()), "UTF-8");
 
-			FileWriter fileWriter = new FileWriter(javaFile, false);
-			fileWriter.write(strWriter.toString());
-			fileWriter.flush();
-			fileWriter.close();
+			out.write(strWriter.toString());
+			out.flush();
+			out.close();
 
 		} catch (Exception err) {
 			err.printStackTrace();
