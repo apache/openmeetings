e8a406f5372d5606058c55269e237c914e921927
Maxim Solodovnik <solomax@apache.org>
2012-03-22 03:18:16 +0000

    OPENMEETINGS-94 is partially fixed
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1303646 13f79535-47bb-0310-9956-ffa450edef68

--- src/app/org/openmeetings/app/rss/LoadAtomRssFeed.java
+++ src/app/org/openmeetings/app/rss/LoadAtomRssFeed.java
@@ -82,7 +82,7 @@ public class LoadAtomRssFeed {
 			conn.setRequestProperty("user-agent",
 					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
 			conn.setRequestProperty("Referer",
-					"http://code.google.com/p/openmeetings/");
+					"http://incubator.apache.org/openmeetings/");
 			conn.connect();
 
 			SAXReader reader = new SAXReader();
