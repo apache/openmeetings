8012d016d0077708d1900658a813191687f6d138
Sebastian Wagner <sebawagner@apache.org>
2012-04-10 17:51:15 +0000

    OPENMEETINGS-122 Fix Navigation Test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1311888 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/openmeetings/test/navi/TestNavi.java
+++ src/org/openmeetings/test/navi/TestNavi.java
@@ -18,50 +18,36 @@
  */
 package org.openmeetings.test.navi;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import org.junit.Test;
+import org.openmeetings.app.data.basic.Navimanagement;
 import org.openmeetings.app.persistence.beans.basic.Naviglobal;
 import org.openmeetings.app.persistence.beans.basic.Navimain;
-import org.openmeetings.app.persistence.beans.basic.Sessiondata;
-import org.openmeetings.app.persistence.beans.domain.Organisation_Users;
-import org.openmeetings.app.persistence.beans.user.Users;
-import org.openmeetings.app.remote.MainService;
 import org.openmeetings.test.AbstractOpenmeetingsSpringTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestNavi extends AbstractOpenmeetingsSpringTest {
+	
 	@Autowired
-	private MainService mService;
+	private Navimanagement navimanagement;
 	
 	@Test
 	public void testGetNavi(){
 		
-		Sessiondata sessionData = mService.getsessiondata();
-		
-		Users us = (Users) mService.loginUser(sessionData.getSession_id(), username, userpass, false, null, -1L);
-		
-		assertNotNull("User is unable to login", us);
-		System.out.println("us: "+ us.getFirstname());
-		
-        for (Organisation_Users ou : us.getOrganisation_users()) {
+        List<Naviglobal> ll = navimanagement.getMainMenu(3L, 1L, 1L);
 
-            Long organization_id = ou.getOrganisation().getOrganisation_id();
-            List<Naviglobal> ll = mService.getNavi(sessionData.getSession_id(), 1, organization_id);
+        assertTrue("GlobalNavi size should be greater than zero: " + ll.size(), ll.size() > 0);
+        System.out.println("NaviGlobal size: " + ll.size());
 
-            assertTrue("GlobalNavi size should be greater than zero: " + ll.size(), ll.size() > 0);
-            System.out.println("NaviGlobal size: " + ll.size());
+        for (Naviglobal navigl : ll) {
+            System.out.println("Naviglobal label: " + navigl.getLabel().getValue());
 
-            for (Naviglobal navigl : ll) {
-                System.out.println("Naviglobal label: " + navigl.getLabel().getValue());
+            for (Navimain navim : navigl.getMainnavi()) {
+                System.out.println("-->" + navim.getLabel().getValue());
 
-                for (Navimain navim : navigl.getMainnavi()) {
-                    System.out.println("-->" + navim.getLabel().getValue());
-
-                }
             }
         }
 		
