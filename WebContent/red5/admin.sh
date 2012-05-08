080d65ea74e4f6291fb0f565b99e6c97a1f75628
Sebastian Wagner <sebawagner@apache.org>
2012-05-08 17:27:35 +0000

    Fixes: Emoticons replaced with few from FamFamFam under Creative Commons Attribution License 3.0 Fix license header in some files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1335653 13f79535-47bb-0310-9956-ffa450edef68

--- WebContent/red5/admin.sh
+++ WebContent/red5/admin.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# #############################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# #############################################
 export RED5_HOME=$(cd $(dirname "$0"); pwd) 
 
 export CLASSPATH=$RED5_HOME/*:$RED5_HOME/conf:$RED5_HOME/lib/*:$RED5_HOME/webapps/openmeetings/WEB-INF/lib/*:$RED5_HOME/webapps/openmeetings/WEB-INF:$RED5_HOME/webapps/openmeetings/WEB-INF/classes
