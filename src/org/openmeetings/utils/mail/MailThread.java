82ba671b8a4c0f7765c366fa697d327c7379c4c0
Maxim Solodovnik <solomax@apache.org>
2012-06-18 10:21:36 +0000

    code cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1351270 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/openmeetings/utils/mail/MailThread.java
+++ src/org/openmeetings/utils/mail/MailThread.java
@@ -23,10 +23,8 @@ import java.util.Properties;
 
 import javax.activation.DataHandler;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
