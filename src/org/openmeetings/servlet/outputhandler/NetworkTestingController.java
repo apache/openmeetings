50affbc22ad5720baed6f17f791410b571555842
German Grekhov <ggrekhov@apache.org>
2012-06-18 18:15:52 +0000

    NetworkTesting: upload speed; 1st gui version; log; exclusive mode
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1351442 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/openmeetings/servlet/outputhandler/NetworkTestingController.java
+++ src/org/openmeetings/servlet/outputhandler/NetworkTestingController.java
@@ -19,8 +19,11 @@
 package org.openmeetings.servlet.outputhandler;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,8 +33,8 @@ import java.io.IOException;
 @Controller
 public class NetworkTestingController {
 
-    @RequestMapping(value = "/NetworkTesting", method = RequestMethod.POST)
-	public void service(HttpServletRequest request, HttpServletResponse httpServletResponse)
+    @RequestMapping(value = "/networktest.upload", method = RequestMethod.POST)
+	public void service(HttpServletRequest request, HttpServletResponse httpServletResponse, HttpSession session)
 			throws ServletException, IOException {
 
     }
