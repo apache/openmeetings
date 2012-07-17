e34c1401a8860b0b05faf0c0258dfc2c0e1a0e6c
Maxim Solodovnik <solomax@apache.org>
2012-07-17 19:38:28 +0000

    OPENMEETINGS-266 only one publishing can be started
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1362620 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/openmeetings/app/remote/red5/ScopeApplicationAdapter.java
+++ src/org/openmeetings/app/remote/red5/ScopeApplicationAdapter.java
@@ -309,7 +309,7 @@ public class ScopeApplicationAdapter extends MultiThreadedApplicationAdapter imp
 				boolean startStreaming = Boolean.valueOf("" + map.get(
 						"startStreaming"));
 				boolean startPublishing = Boolean.valueOf("" + map.get(
-						"startPublishing"));
+						"startPublishing")) && (0 == clientListManager.getPublisingCount(currentClient.getRoom_id()));
 
 				currentClient.setRoom_id(Long.parseLong(current.getScope()
 						.getName()));
