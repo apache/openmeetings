b8f7ad5d463ce45d2445f8137830e6212539bac0
Maxim Solodovnik <solomax@apache.org>
2012-10-18 14:15:44 +0000

    Wicket: code is modified to call omComponentInit on every reload
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openmeetings/trunk/singlewebapp@1399671 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/openmeetings/web/components/MenuPanel.java
+++ src/org/apache/openmeetings/web/components/MenuPanel.java
@@ -150,7 +150,7 @@ public class MenuPanel extends BasePanel {
 										target.add(contents.replace(new ServersPanel("child")));
 										break;
 								}
-								target.appendJavaScript("location.hash = '" + JavaScriptUtils.escapeQuotes(hash) + "'; if (typeof window.omComponentInit == 'function'){ omComponentInit(); window.omComponentInit = null; };");
+								target.appendJavaScript("location.hash = '" + JavaScriptUtils.escapeQuotes(hash) + "'; if (typeof window.omComponentInit == 'function'){ omComponentInit(); };");
 							};
 						}.add(AttributeModifier.replace("href", hash)));
 					}
