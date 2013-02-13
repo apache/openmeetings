3eaae519cd62a0ff5425ebc821db90fea514543e
Maxim Solodovnik <solomax@apache.org>
2013-02-13 16:27:13 +0000

    Import: additional backups are added to the test;      assertions are updated;
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1445716 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/openmeetings/test/backup/TestOldBackups.java
+++ src/org/apache/openmeetings/test/backup/TestOldBackups.java
@@ -6,21 +6,39 @@ import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.FileInputStream;
 
+import org.apache.openmeetings.OpenmeetingsVariables;
+import org.apache.openmeetings.data.calendar.daos.AppointmentDao;
+import org.apache.openmeetings.data.calendar.daos.MeetingMemberDao;
+import org.apache.openmeetings.data.conference.RoomManager;
+import org.apache.openmeetings.data.conference.dao.RoomDao;
 import org.apache.openmeetings.data.file.dao.FileExplorerItemDao;
-import org.apache.openmeetings.data.flvrecord.FlvRecordingDao;
+import org.apache.openmeetings.data.user.dao.OrganisationDao;
 import org.apache.openmeetings.data.user.dao.UsersDao;
 import org.apache.openmeetings.servlet.outputhandler.BackupImportController;
 import org.apache.openmeetings.test.AbstractOpenmeetingsSpringTest;
 import org.junit.Test;
+import org.red5.logging.Red5LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestOldBackups extends AbstractOpenmeetingsSpringTest {
+	private static final Logger log = Red5LoggerFactory.getLogger(
+			TestOldBackups.class, OpenmeetingsVariables.webAppRootKey);
+	
 	@Autowired
 	private BackupImportController backupController;
 	@Autowired
+	private OrganisationDao organisationDao;
+	@Autowired
 	private UsersDao usersDao;
 	@Autowired
-	private FlvRecordingDao recordingDao;
+	private RoomDao roomDao;
+	@Autowired
+	private RoomManager roomManager;
+	@Autowired
+	private AppointmentDao appointmentDao;
+	@Autowired
+	private MeetingMemberDao meetingMemberDao;
 	@Autowired
 	private FileExplorerItemDao fileExplorerItemDao;
 
@@ -32,25 +50,39 @@ public class TestOldBackups extends AbstractOpenmeetingsSpringTest {
 		if (!backupsHome.exists() || !backupsHome.isDirectory()) {
 			fail("Invalid directory is specified for backup files: " + backupsDir);
 		}
+		long orgCount = 0;
 		long userCount = 0;
-		long recCount = 0;
-		long fileCount = 0;
+		long roomCount = 0;
+		long roomOrgCount = 0;
+		long apptCount = 0;
+		long meetingMembersCount = 0;
 		for (File backup : backupsHome.listFiles()) {
+			log.debug("Import of backup file : '" + backup.getName() + "' is started ...");
 			try {
 				backupController.performImport(new FileInputStream(backup));
+				long newOrgCount = organisationDao.count();
 				long newUserCount = usersDao.count();
-				long newRecCount = recordingDao.getFlvRecordings().size();
-				long newFileCount = fileExplorerItemDao.getFileExplorerItems().size();
+				long newRoomCount = roomDao.count();
+				long newRoomOrgCount = roomManager.getRoomsOrganisations().size();
+				long newApptCount = appointmentDao.getAppointments().size();
+				long newMeetingMembersCount = meetingMemberDao.getMeetingMembers().size();
+				assertTrue("Zero organizations were imported", newOrgCount > orgCount);
 				assertTrue("Zero users were imported", newUserCount > userCount);
-				assertTrue("Zero recordings were imported", newRecCount > recCount);
-				assertTrue("Zero files were imported", newFileCount > fileCount);
-
+				assertTrue("Zero rooms were imported", newRoomCount > roomCount);
+				assertTrue("Zero room organizations were imported", newRoomOrgCount > roomOrgCount);
+				assertTrue("Zero appointments were imported", newApptCount > apptCount);
+				assertTrue("Zero meeting members were imported", newMeetingMembersCount > meetingMembersCount);
+				
+				orgCount = newOrgCount;
 				userCount = newUserCount;
-				recCount = newRecCount;
-				fileCount = newFileCount;
+				roomCount = newRoomCount;
+				roomOrgCount = newRoomOrgCount;
+				apptCount = newApptCount;
+				meetingMembersCount = newMeetingMembersCount;
 			} catch (Exception e) {
 				throw new RuntimeException("Unexpected exception while importing backup: " + backup.getName(), e);
 			}
+			log.debug("... Done.");
 		}
 	}
 }
