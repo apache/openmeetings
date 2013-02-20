89ec3a663fa77c58953094583446ba13f46676fb
Sebastian Wagner <sebawagner@apache.org>
2013-02-20 07:58:55 +0000

    change to "LIKE" instead of ILIKE
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1448026 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/openmeetings/persistence/beans/user/State.java
+++ src/org/apache/openmeetings/persistence/beans/user/State.java
@@ -36,7 +36,7 @@ import javax.persistence.Table;
 			"where c.state_id = :state_id AND c.deleted <> :deleted"),
 	@NamedQuery(name = "getStates", query = "select c from State as c where c.deleted <> :deleted"),
 	@NamedQuery(name = "getStateByName", query = "select c from State as c " +
-			"where c.name ILIKE :name AND c.deleted <> :deleted")
+			"where c.name LIKE :name AND c.deleted <> :deleted")
 })
 @Table(name = "state")
 public class State implements Serializable {
