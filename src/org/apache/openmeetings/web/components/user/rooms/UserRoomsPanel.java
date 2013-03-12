caa977bb1982dac3917d11af56d81b44380cee08
Maxim Solodovnik <solomax@apache.org>
2013-03-12 06:01:06 +0000

    HTML5: list of public rooms with refreshable user count is added;
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1455412 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/openmeetings/web/components/user/rooms/UserRoomsPanel.java
+++ src/org/apache/openmeetings/web/components/user/rooms/UserRoomsPanel.java
@@ -18,10 +18,23 @@
  */
 package org.apache.openmeetings.web.components.user.rooms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.openmeetings.data.conference.dao.RoomDao;
+import org.apache.openmeetings.persistence.beans.room.Room;
+import org.apache.openmeetings.session.SessionManager;
+import org.apache.openmeetings.web.app.Application;
 import org.apache.openmeetings.web.app.WebSession;
 import org.apache.openmeetings.web.components.MenuPanel.MenuParams;
 import org.apache.openmeetings.web.components.UserPanel;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 public class UserRoomsPanel extends UserPanel {
 	private static final long serialVersionUID = -3246259803623925341L;
@@ -29,18 +42,52 @@ public class UserRoomsPanel extends UserPanel {
 	public UserRoomsPanel(String id, MenuParams param) {
 		super(id);
 
-		long title = 777L;
+		long title, desc;
+		List<Room> rooms;
 		switch (param) {
-		case myTabButton:
-			title = 781L;
-			break;
-		case privateTabButton:
-			title = 779L;
-			break;
-		case publicTabButton:
-		default:
-			break;
+			case myTabButton:
+				title = 781L;
+				desc = 782L;
+				//rooms = Application.getBean(RoomDao.class).getPublicRooms();
+				//FIXME 2 !!!! fake rooms appointmentLogic.getTodaysAppointmentsForUser(users_id);
+				rooms = new ArrayList<Room>();
+				break;
+			case privateTabButton:
+				title = 779L;
+				desc = 780L;
+				//FIXME getRoomsOrganisationByOrganisationId
+				rooms = new ArrayList<Room>();
+				break;
+			case publicTabButton:
+			default:
+				title = 777L;
+				desc = 778L;
+				rooms = Application.getBean(RoomDao.class).getPublicRooms();
+				break;
 		}
 		add(new Label("title", WebSession.getString(title)));
+		add(new Label("desc", WebSession.getString(desc)));
+		add(new ListView<Room>("list", rooms) {
+			private static final long serialVersionUID = 9189085478336224890L;
+
+			@Override
+			protected void populateItem(ListItem<Room> item) {
+				final Room r = item.getModelObject();
+				item.add(new Label("roomName", r.getName()));
+				final IModel<Integer> curUsersModel = new Model<Integer>(Application.getBean(SessionManager.class).getClientListByRoom(r.getRooms_id()).size()); 
+				final Label curUsers = new Label("curUsers", curUsersModel);
+				item.add(curUsers.setOutputMarkupId(true));
+				item.add(new Label("totalUsers", r.getNumberOfPartizipants()));
+				item.add(new AjaxLink<Void>("refresh") {
+					private static final long serialVersionUID = -3426813755917489787L;
+
+					@Override
+					public void onClick(AjaxRequestTarget target) {
+						curUsersModel.setObject(Application.getBean(SessionManager.class).getClientListByRoom(r.getRooms_id()).size());
+						target.add(curUsers);
+					}
+				});
+			}
+		});
 	}
 }
