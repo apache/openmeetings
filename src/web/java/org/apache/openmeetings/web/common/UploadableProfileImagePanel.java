8971ac8988c08f7bf82bf1b9322f891665aeefe5
Maxim Solodovnik <solomax@apache.org>
2013-12-26 18:30:11 +0000

    [OPENMEETINGS-865] Picture upload
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1553544 13f79535-47bb-0310-9956-ffa450edef68

--- src/web/java/org/apache/openmeetings/web/common/UploadableProfileImagePanel.java
+++ src/web/java/org/apache/openmeetings/web/common/UploadableProfileImagePanel.java
@@ -30,9 +30,10 @@ import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.extensions.ajax.markup.html.form.upload.UploadProgressBar;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.upload.FileUpload;
-import org.apache.wicket.markup.html.form.upload.FileUploadField;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.util.lang.Bytes;
+
+import de.agilecoders.wicket.extensions.javascript.jasny.FileUploadField;
 
 public class UploadableProfileImagePanel extends ProfileImagePanel {
 	private static final long serialVersionUID = -6681473769070950510L;
