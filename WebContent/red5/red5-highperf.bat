e38063cf37f6a75975f407f93104b3596a50f5c0
Maxim Solodovnik <solomax@apache.org>
2014-01-26 04:08:31 +0000

    Spanish translation is updated; Highperf scripts are fixed
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1561446 13f79535-47bb-0310-9956-ffa450edef68

--- WebContent/red5/red5-highperf.bat
+++ WebContent/red5/red5-highperf.bat
@@ -15,7 +15,7 @@ REM #############################################
 
 if NOT DEFINED RED5_HOME set RED5_HOME=%~dp0
 
-set JAVA_OPTS= -Xmx768m -Xms512m -Xmn512m -Xss256k -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:ParallelGCThreads=4 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=16 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=31 -Djava.net.preferIPv4Stack=true
+set JAVA_OPTS= -Xmx768m -Xms512m -Xmn512m -Xss512k -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:ParallelGCThreads=4 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:SurvivorRatio=16 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=31 -Djava.net.preferIPv4Stack=true
 
 echo Running High Performance Red5
 start cmd /c "%RED5_HOME%\red5.bat" >> "%RED5_HOME%\log\jvm.stdout"
