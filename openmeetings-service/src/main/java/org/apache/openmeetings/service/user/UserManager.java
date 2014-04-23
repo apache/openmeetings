9d5ee2cb4398f58f8b985ddf22d0f781fcf45b7c
Vasiliy Degtyarev <vdegtyarev@apache.org>
2014-04-23 11:00:39 +0000

    Merged revision(s) 1589367 from openmeetings/branches/3.0.x/src/main/java/org/apache/openmeetings/data/user/UserManager.java: OPENMEETINGS-983 is fixed. Verification URL is generated correctly. ........
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1589369 13f79535-47bb-0310-9956-ffa450edef68

--- openmeetings-service/src/main/java/org/apache/openmeetings/service/user/UserManager.java
+++ openmeetings-service/src/main/java/org/apache/openmeetings/service/user/UserManager.java
@@ -653,8 +653,8 @@ public class UserManager implements IUserManager {
 					if (hash == null){
 						hash = ManageCryptStyle.getInstanceOfCrypt().createPassPhrase(login
 								+ CalendarPatterns.getDateWithTimeByMiliSeconds(new Date()));
-						link += "activateUser?u=" + hash;
 					}
+					link += "activate?u=" + hash;
 
 					if (sendWelcomeMessage && email.length() != 0) {
 						String sendMail = emailManagement.sendMail(login, password, email, link, sendConfirmation);
