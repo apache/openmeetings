a5eb4f686c334fc185f055e864a1109f8c518319
Maxim Solodovnik <solomax@apache.org>
2014-10-02 01:41:35 +0000

    Backup import is fixed
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1628875 13f79535-47bb-0310-9956-ffa450edef68

--- openmeetings-install/src/main/java/org/apache/openmeetings/backup/BackupImport.java
+++ openmeetings-install/src/main/java/org/apache/openmeetings/backup/BackupImport.java
@@ -366,7 +366,7 @@ public class BackupImport {
 			
 			List<RoomOrganisation> list = readList(serializer, f, "rooms_organisation.xml", "room_organisations", RoomOrganisation.class);
 			for (RoomOrganisation ro : list) {
-				if (!ro.getDeleted()) {
+				if (!ro.getDeleted() && ro.getRoom() != null && ro.getOrganisation() != null && ro.getOrganisation().getId() != null) {
 					// We need to reset this as openJPA reject to store them otherwise
 					ro.setId(null);
 					roomOrganisationDao.update(ro, null);
