9affb26416a1ba02741db62a50154a4d67aee555
Maxim Solodovnik <solomax@apache.org>
2014-11-27 08:10:45 +0000

    Translations are updated; site is updated
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1642041 13f79535-47bb-0310-9956-ffa450edef68

--- openmeetings-server/src/site/resources/js/site.js
+++ openmeetings-server/src/site/resources/js/site.js
@@ -59,9 +59,8 @@ $(document).ready(function() {
 		c.attr('data-ride', 'carousel').addClass('carousel slide');
 	}
 	// "New" markers
-	var topics = ["We need you", "Call For Logo"];
+	var topics = ["Call For Logo"];
 	for (var i = 0; i < topics.length; ++i) {
 		$('ul.nav.nav-list ul.nav.nav-list li:contains("' + topics[i] + '") a').append('&nbsp;&nbsp;<span class="label label-success">New</span>');
 	}
-	$('ul.breadcrumb li:contains("We need you") a').prepend('&nbsp;&nbsp;<span class="label label-success">New</span>');
 })
