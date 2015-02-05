e167c2fd34da3ef877d27b84f2c08fd42763d5ac
Maxim Solodovnik <solomax@apache.org>
2015-02-05 04:40:08 +0000

    [OPENMEETINGS-980, OPENMEETINGS-1126, OPENMEETINGS-1131, OPENMEETINGS-1139] wicket and wicket-jquery-ui libraries are updated, calendar is updated to use LocalDateTime; code clean-up
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1657475 13f79535-47bb-0310-9956-ffa450edef68

---
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/util/CalendarHelper.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License") +  you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.openmeetings.web.util;
+
+import static org.apache.openmeetings.web.app.WebSession.getUserTimeZone;
+
+import java.util.Date;
+
+import org.threeten.bp.Instant;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.ZoneId;
+
+public class CalendarHelper {
+	public static ZoneId getZoneId() {
+		return ZoneId.of(getUserTimeZone().getID());
+	}
+	
+	public static Date getDate(LocalDate d) {
+		return getDate(d.atStartOfDay());
+	}
+
+	public static Date getDate(LocalDateTime d) {
+		return new Date(d.atZone(getZoneId()).toInstant().toEpochMilli());
+	}
+
+	public static LocalDateTime getDate(Date d) {
+		return Instant.ofEpochMilli(d.getTime()).atZone(ZoneId.of(getUserTimeZone().getID())).toLocalDateTime();
+	}
+}
