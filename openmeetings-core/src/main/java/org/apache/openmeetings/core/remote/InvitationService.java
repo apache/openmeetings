593436c73d317dfe198dda1cf7554eb8cdf60614
Maxim Solodovnik <solomax@apache.org>
2015-03-12 10:00:29 +0000

    [OPENMEETINGS-1187] 'Send One-Time Invitation' function works as expected
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/trunk/singlewebapp@1666115 13f79535-47bb-0310-9956-ffa450edef68

--- openmeetings-core/src/main/java/org/apache/openmeetings/core/remote/InvitationService.java
+++ openmeetings-core/src/main/java/org/apache/openmeetings/core/remote/InvitationService.java
@@ -146,7 +146,7 @@ public class InvitationService implements IPendingServiceCallback {
 		Long users_id = sessiondataDao.checkSession(SID);
 
 		if (AuthLevelUtil.hasUserLevel(userDao.getRights(users_id))) {
-			Invitation inv = (Invitation)invitationManager.getInvitationByHashCode(invitationHash, true);
+			Invitation inv = (Invitation)invitationManager.getInvitationByHashCode(invitationHash, false);
 			inv.getInvitee().setLanguageId(language_id);
 			invitationManager.sendInvitionLink(inv, MessageType.Create, subject, message, false);
 		} else {
