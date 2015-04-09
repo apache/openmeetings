3cb2873a5250048b62faec382cf6d46f800d3d87
Maxim Solodovnik <solomax@apache.org>
2015-04-09 11:21:12 +0000

    [OPENMEETINGS-1168] message counter is updated on message delete
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/branches/3.0.x@1672294 13f79535-47bb-0310-9956-ffa450edef68

--- src/web/java/org/apache/openmeetings/web/user/profile/MessagesContactsPanel.java
+++ src/web/java/org/apache/openmeetings/web/user/profile/MessagesContactsPanel.java
@@ -212,6 +212,9 @@ public class MessagesContactsPanel extends UserPanel {
 		selectedMessages.clear();
 		selectMessage(-1, target);
 		unread.setDefaultModelObject(getBean(PrivateMessagesDao.class).count(getUserId(), selectedFolderModel.getObject(), null));
+		if (target != null) {
+			target.add(unread);
+		}
 	}
 	
 	private String getDisplayName(User u) {
