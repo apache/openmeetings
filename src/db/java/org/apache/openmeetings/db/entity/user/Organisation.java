5a980d6104da1350722839db70d87cf8c39fdb11
Maxim Solodovnik <solomax@apache.org>
2015-05-18 11:04:02 +0000

    Import is fixed to work with organisations with empty names
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/branches/3.0.x@1679986 13f79535-47bb-0310-9956-ffa450edef68

--- src/db/java/org/apache/openmeetings/db/entity/user/Organisation.java
+++ src/db/java/org/apache/openmeetings/db/entity/user/Organisation.java
@@ -59,7 +59,7 @@ public class Organisation implements Serializable, IDataProviderEntity {
 	private Long organisation_id;
 	
 	@Column(name="name")
-	@Element(data=true)
+	@Element(data = true, required = false)
 	private String name;
 	
 	@Column(name="insertedby")
