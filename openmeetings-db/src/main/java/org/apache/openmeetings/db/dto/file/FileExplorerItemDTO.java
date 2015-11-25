5a1bab8857d9779a34495046dc0bc825f082a57a
Maxim Solodovnik <solomax@apache.org>
2015-11-25 14:53:32 +0000

    [OPENMEETINGS-1118] fileItemDTO is improved
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/branches/3.1.x@1716429 13f79535-47bb-0310-9956-ffa450edef68

--- openmeetings-db/src/main/java/org/apache/openmeetings/db/dto/file/FileExplorerItemDTO.java
+++ openmeetings-db/src/main/java/org/apache/openmeetings/db/dto/file/FileExplorerItemDTO.java
@@ -41,36 +41,45 @@ public class FileExplorerItemDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Long id;
 	private String name;
+	private String hash;
 	private Long parentId;
 	private Long roomId;
 	private Long size;
 	private String externalId;
 	private String externalType;
 	private Type type;
+	private Integer flvWidth;
+	private Integer flvHeight;
 
 	public FileExplorerItemDTO() {}
 
 	public FileExplorerItemDTO(FileExplorerItem f) {
 		id = f.getId();
 		name = f.getName();
+		hash = f.getHash();
 		parentId = f.getParentId();
 		roomId = f.getRoomId();
 		size = f.getSize();
 		externalId = f.getExternalId();
 		externalType = f.getExternalType();
 		type = f.getType();
+		flvWidth = f.getFlvWidth();
+		flvHeight = f.getFlvHeight();
 	}
 	
 	public FileExplorerItem get() {
 		FileExplorerItem f = new FileExplorerItem();
 		f.setId(id);
 		f.setName(name);
+		f.setHash(hash);
 		f.setParentId(parentId);
 		f.setRoomId(roomId);
 		f.setSize(size);
 		f.setExternalId(externalId);
 		f.setExternalType(externalType);
 		f.setType(type);
+		f.setFlvWidth(flvWidth);
+		f.setFlvHeight(flvHeight);
 		return f;
 	}
 	
@@ -81,12 +90,21 @@ public class FileExplorerItemDTO implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String fileName) {
-		this.name = fileName;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
 	}
 
 	public Long getParentId() {
@@ -135,6 +153,22 @@ public class FileExplorerItemDTO implements Serializable {
 
 	public void setType(Type type) {
 		this.type = type;
+	}
+
+	public Integer getFlvWidth() {
+		return flvWidth;
+	}
+
+	public void setFlvWidth(Integer flvWidth) {
+		this.flvWidth = flvWidth;
+	}
+
+	public Integer getFlvHeight() {
+		return flvHeight;
+	}
+
+	public void setFlvHeight(Integer flvHeight) {
+		this.flvHeight = flvHeight;
 	}
 
 	public static List<FileExplorerItemDTO> list(List<FileExplorerItem> l) {
