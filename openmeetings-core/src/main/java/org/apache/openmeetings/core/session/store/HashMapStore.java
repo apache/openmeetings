267c8d7f7d5c00d9792376a0bc35215594627cda
Maxim Solodovnik <solomax@apache.org>
2016-03-31 13:27:59 +0000

    [OPENMEETINGS-1350] cluster is partially fixed
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/application/branches/3.1.x@1737236 13f79535-47bb-0310-9956-ffa450edef68

--- openmeetings-core/src/main/java/org/apache/openmeetings/core/session/store/HashMapStore.java
+++ openmeetings-core/src/main/java/org/apache/openmeetings/core/session/store/HashMapStore.java
@@ -18,17 +18,19 @@
  */
 package org.apache.openmeetings.core.session.store;
 
+import static org.apache.openmeetings.util.OpenmeetingsVariables.webAppRootKey;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.openmeetings.db.entity.room.Client;
 import org.apache.openmeetings.db.entity.server.Server;
-import org.apache.openmeetings.util.OpenmeetingsVariables;
 import org.red5.logging.Red5LoggerFactory;
 import org.slf4j.Logger;
 
@@ -48,14 +50,12 @@ import org.slf4j.Logger;
  * 
  */
 public class HashMapStore implements IClientPersistenceStore {
-	protected static final Logger log = Red5LoggerFactory.getLogger(
-			HashMapStore.class, OpenmeetingsVariables.webAppRootKey);
+	protected static final Logger log = Red5LoggerFactory.getLogger(HashMapStore.class, webAppRootKey);
 	
-	
-	private LinkedHashMap<String, Client> clientsByStreamId = new LinkedHashMap<String, Client>();
+	private Map<String, Client> clientsByStreamId = new ConcurrentHashMap<>();
 
-	public void clearCache() {
-		clientsByStreamId = new LinkedHashMap<String, Client>();
+	public void clear() {
+		clientsByStreamId = new ConcurrentHashMap<>();
 	}
 	
 	public void put(String streamId, Client rcl) {
@@ -71,8 +71,9 @@ public class HashMapStore implements IClientPersistenceStore {
 	}
 	
 	public List<Client> getClientsByPublicSID(Server server, String publicSID) {
-		List<Client> clientList = new ArrayList<Client>();
-		for (Client cl : clientsByStreamId.values()) {
+		List<Client> clientList = new ArrayList<>();
+		for (Map.Entry<String, Client> e: clientsByStreamId.entrySet()) {
+			Client cl = e.getValue();
 			if (cl.getPublicSID().equals(publicSID)) {
 				clientList.add(cl);
 			}
@@ -81,9 +82,10 @@ public class HashMapStore implements IClientPersistenceStore {
 	}
 	
 	public Map<Long,List<Client>> getClientsByPublicSID(String publicSID) {
-		Map<Long,List<Client>> clientMapList = new HashMap<Long,List<Client>>();
-		List<Client> clientList = new ArrayList<Client>();
-		for (Client cl : clientsByStreamId.values()) {
+		Map<Long,List<Client>> clientMapList = new HashMap<>();
+		List<Client> clientList = new ArrayList<>();
+		for (Map.Entry<String, Client> e: clientsByStreamId.entrySet()) {
+			Client cl = e.getValue();
 			if (cl.getPublicSID().equals(publicSID)) {
 				clientList.add(cl);
 			}
@@ -107,8 +109,9 @@ public class HashMapStore implements IClientPersistenceStore {
 	}
 	
 	public List<Client> getClientsByUserId(Server server, Long userId) {
-		List<Client> clientList = new ArrayList<Client>();
-		for (Client cl : clientsByStreamId.values()) {
+		List<Client> clientList = new ArrayList<>();
+		for (Map.Entry<String, Client> e: clientsByStreamId.entrySet()) {
+			Client cl = e.getValue();
 			if (cl.getUserId().equals(userId)) {
 				clientList.add(cl);
 			}
@@ -117,8 +120,9 @@ public class HashMapStore implements IClientPersistenceStore {
 	}
 	
 	public  List<Client> getClientsByRoomId(Long roomId) {
-		List<Client> clientList = new ArrayList<Client>();
-		for (Client cl : clientsByStreamId.values()) {
+		List<Client> clientList = new ArrayList<>();
+		for (Map.Entry<String, Client> e: clientsByStreamId.entrySet()) {
+			Client cl = e.getValue();
 			if (cl.getRoomId() != null && cl.getRoomId().equals(roomId)) {
 				clientList.add(cl);
 			}
@@ -146,7 +150,6 @@ public class HashMapStore implements IClientPersistenceStore {
 		return clientsByStreamId.size();
 	}
 	
-	
 	/**
 	 * Print some session statistics to the debug out
 	 * 
@@ -159,12 +162,10 @@ public class HashMapStore implements IClientPersistenceStore {
 	}
 
 	public String getDebugInformation(List<DEBUG_DETAILS> detailLevel) {
-
 		StringBuilder statistics = new StringBuilder();
 
 		if (detailLevel.contains(DEBUG_DETAILS.SIZE)) {
-			addNewLine(statistics, "Number of sessions Total "
-					+ getTotalNumberOfSessions());
+			addNewLine(statistics, "Number of sessions Total " + getTotalNumberOfSessions());
 		}
 
 		return statistics.toString();
@@ -175,8 +176,9 @@ public class HashMapStore implements IClientPersistenceStore {
 	}
 
 	public List<Long> getRoomsIdsByServer(Server server) {
-		HashSet<Long> rooms = new HashSet<Long>();
-		for (Client cl : clientsByStreamId.values()) {
+		Set<Long> rooms = new HashSet<>();
+		for (Map.Entry<String, Client> e: clientsByStreamId.entrySet()) {
+			Client cl = e.getValue();
 			Long roomId = cl.getRoomId();
 			if (roomId != null && roomId > 0 && !rooms.contains(roomId)) {
 				rooms.add(roomId);
