cad05d1dd81a643e5183d7305ec5dcfc409db773
Maxim Solodovnik <solomax@apache.org>
2016-08-06 06:50:11 +0000

    [OPENMEETINGS-1444] Language editor is fixed
    git-svn-id: https://svn.apache.org/repos/asf/openmeetings/application/trunk@1755388 13f79535-47bb-0310-9956-ffa450edef68

--- openmeetings-db/src/main/java/org/apache/openmeetings/db/dao/label/LabelDao.java
+++ openmeetings-db/src/main/java/org/apache/openmeetings/db/dao/label/LabelDao.java
@@ -253,12 +253,13 @@ public class LabelDao implements IDataProviderDao<StringLabel>{
 	}
 	
 	private static List<StringLabel> getLabels(Locale l, final String search) {
-		List<StringLabel> result = null;
-		if (labelCache.containsKey(l)) {
-			result = labelCache.get(l);
-		} else {
-			result = labelCache.putIfAbsent(l, getLabels(l));
+		if (!labelCache.containsKey(l)) {
+			List<StringLabel> ll = getLabels(l);
+			if (ll != null) {
+				labelCache.putIfAbsent(l, ll);
+			}
 		}
+		List<StringLabel> result = labelCache.containsKey(l) ? labelCache.get(l) : new ArrayList<StringLabel>();
 		if (!Strings.isEmpty(search)) {
 			CollectionUtils.filter(result, new Predicate<StringLabel>() {
 				@Override
