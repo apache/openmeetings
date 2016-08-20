057890756413f7064ad8c5b4be83bc12981aee4c
Maxim Solodovnik <solomax@apache.org>
2016-08-20 00:17:15 +0000

    [OPENMEETINGS-1138] more generalization

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/user/record/RecordingInvitationForm.java
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/user/record/RecordingInvitationForm.java
@@ -18,52 +18,16 @@
  */
 package org.apache.openmeetings.web.user.record;
 
-import static org.apache.openmeetings.util.OpenmeetingsVariables.webAppRootKey;
-import static org.apache.openmeetings.web.app.Application.getBean;
-import static org.apache.openmeetings.web.app.Application.getInvitationLink;
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-import org.apache.openmeetings.db.dao.basic.ConfigurationDao;
-import org.apache.openmeetings.db.entity.room.Invitation;
-import org.apache.openmeetings.db.entity.room.Invitation.MessageType;
-import org.apache.openmeetings.db.entity.user.User;
-import org.apache.openmeetings.service.room.InvitationManager;
-import org.apache.openmeetings.web.app.Application;
 import org.apache.openmeetings.web.common.InvitationForm;
-import org.apache.openmeetings.web.util.UserMultiChoice;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.util.CollectionModel;
-import org.apache.wicket.util.string.Strings;
-import org.red5.logging.Red5LoggerFactory;
-import org.slf4j.Logger;
 
 public class RecordingInvitationForm extends InvitationForm {
 	private static final long serialVersionUID = 1L;
-	private static final Logger log = Red5LoggerFactory.getLogger(RecordingInvitationForm.class, webAppRootKey);
 	private Long recordingId;
-	final UserMultiChoice recipients = new UserMultiChoice("recipients", new CollectionModel<User>(new ArrayList<User>()));
 
 	public RecordingInvitationForm(String id) {
 		super(id);
-		add(recipients.setLabel(Model.of(Application.getString(216))).setRequired(true).add(new AjaxFormComponentUpdatingBehavior("change") {
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			protected void onUpdate(AjaxRequestTarget target) {
-				url.setModelObject(null);
-				updateButtons(target);
-			}
-		}).setOutputMarkupId(true));
-	}
-
-	private void updateButtons(AjaxRequestTarget target) {
-		Collection<User> to = recipients.getModelObject();
-		dialog.send.setEnabled(to.size() > 0, target);
-		dialog.generate.setEnabled(to.size() == 1, target);
+		add(recipients);
 	}
 
 	@Override
@@ -71,39 +35,6 @@ public class RecordingInvitationForm extends InvitationForm {
 		super.updateModel(target);
 		//Invitation i = getModelObject();
 		//i.setReco
-		recipients.setModelObject(new ArrayList<User>());
-		recipients.setEnabled(true);
-	}
-
-	@Override
-	public boolean onSubmit(AjaxRequestTarget target, boolean generate, boolean send) {
-		//TODO need to be reviewed
-		if (generate) {
-			Invitation i = create(recipients.getModelObject().iterator().next());
-			setModelObject(i);
-			url.setModelObject(getInvitationLink(getBean(ConfigurationDao.class).getBaseUrl(), i));
-			target.add(url);
-			return true;
-		} else if (send) {
-			if (Strings.isEmpty(url.getModelObject())) {
-				for (User u : recipients.getModelObject()) {
-					Invitation i = create(u);
-					try {
-						getBean(InvitationManager.class).sendInvitationLink(i, MessageType.Create, subject.getModelObject(), message.getModelObject(), false);
-					} catch (Exception e) {
-						log.error("error while sending invitation by User ", e);
-					}
-				}
-			} else {
-				Invitation i = getModelObject();
-				try {
-					getBean(InvitationManager.class).sendInvitationLink(i, MessageType.Create, subject.getModelObject(), message.getModelObject(), false);
-				} catch (Exception e) {
-					log.error("error while sending invitation by URL ", e);
-				}
-			}
-		}
-		return false;
 	}
 
 	public void setRecordingId(Long recordingId) {
