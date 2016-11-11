747708d3731741fc6c63702990c67716ab25c83c
Maxim Solodovnik <solomax@apache.org>
2016-11-11 03:32:41 +0000

    no jira: workaround for WICKET 6232

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/common/MainPanel.java
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/common/MainPanel.java
@@ -120,7 +120,8 @@ public class MainPanel extends Panel {
 							@Override
 							public void run() {
 								try {
-									wsConnection.sendMessage(new byte[1], 0, 1);
+									//wsConnection.sendMessage(new byte[1], 0, 1); //FIXME TODO commented for now
+									wsConnection.sendMessage("ping");
 								} catch (IOException e) {
 									log.error("Error while sending ping message to room", e);
 								}
