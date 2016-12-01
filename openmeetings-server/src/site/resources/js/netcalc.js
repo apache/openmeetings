7fb2be539140a4a9eeeafc0295271e0ccbff9729
Maxim Solodovnik <solomax@apache.org>
2016-12-01 04:22:52 +0000

    [OPENMEETINGS-1515] site is now generated using reflow skin

--- openmeetings-server/src/site/resources/js/netcalc.js
+++ openmeetings-server/src/site/resources/js/netcalc.js
@@ -20,121 +20,113 @@
 * limitations under the License.
 */
 
+$(document).ready(function() {
+	$("#sib").val("392");
+	$("#sob").val("392");
+	$("#uib").val("392");
+	$("#uob").val("196");
 
+	$("#amount_cam_res").val(
+			"Cam resolution: 120x90 [4:3 (~196 kbit/s)]");
+	$("#amount_pwv").val("2 participants with audio/video");
+	$("#amount_pnv").val("Participants without audio/video?");
 
-$(document).ready( function() {
-    $("#sib").val("392");
-    $("#sob").val("392");
-    $("#uib").val("392");
-    $("#uob").val("196");
+	$(function() {
+		var p = {
+			0 : "None (Only audio ~44 kbit/s)",
+			1 : "Cam resolution: 40x30 [4:3 (~83 kbit/s)]",
+			2 : "Cam resolution: 80x60 [4:3 (~129 kbit/s)]",
+			3 : "Cam resolution: 120x90 [4:3 (~196 kbit/s)]",
+			4 : "Cam resolution: 160x120 [QQVGA 4:3 (~246 kbit/s)]",
+			5 : "Cam resolution: 240x180 [4:3 (~251 kbit/s)]",
+			6 : "Cam resolution: 320x240 [HVGA 4:3 (~252 kbit/s)]",
+			7 : "Cam resolution: 480x360 [4:3  (~469 kbit/s)]",
+			8 : "Cam resolution: 640x480 [4:3 (~904 kbit/s)]",
+			9 : "Cam resolution: 1024x768 [XGA 4:3 (~2090 kbit/s)]",
+			10 : "Cam resolution: 256x150 [16:9 (~252 kbit/s)]",
+			11 : "Cam resolution: 432x240 [WQVGA 9:5 (~283 kbit/s)]",
+			12 : "Cam resolution: 480x234 [pseudo 16:9 (~302 kbit/s)]",
+			13 : "Cam resolution: 512x300 [16:9 (~411 kbit/s)]",
+			14 : "Cam resolution: 640x360 [nHD 16:9 (~615 kbit/s)]",
+			15 : "Cam resolution: 1024x600 [16:9 (~1624 kbit/s)] ",
+		};
+		var t = {
+			0 : "44",
+			1 : "83",
+			2 : "129",
+			3 : "196",
+			4 : "246",
+			5 : "251",
+			6 : "252",
+			7 : "469",
+			8 : "904",
+			9 : "2090",
+			10 : "252",
+			11 : "283",
+			12 : "302",
+			13 : "411",
+			14 : "615",
+			15 : "1624",
+		}
 
+		$("#slider-cam-res").slider({
+			value : "3",
+			min : 0,
+			max : 15,
+			step : 1,
+			slide : function(event, ui) {
+				$("#cam_res").val(t[ui.value]);
+				$("#amount_cam_res").val(p[ui.value]);
+				var camRes = $("#cam_res").val();
+				var numWithCam = $("#pwv").val();
+				var numWithoutCam = $("#pnv").val();
+				$("#sib").val(+camRes * +numWithCam);
+				$("#sob").val(+camRes * +numWithCam * (+numWithCam + +numWithoutCam - 1));
+				$("#uib").val(+camRes * +numWithCam);
+				$("#uob").val(+camRes);
+			}
+		});
 
-    $("#amount_cam_res").val("Cam resolution: 120x90 [4:3 (~196 kbit/s)]");
-    $("#amount_pwv").val("2 participants with audio/video");
-    $("#amount_pnv").val("Participants without audio/video?");
+		$("#slider-num-with-cam").slider({
+			value : "2",
+			min : 0,
+			max : 100,
+			step : 1,
+			slide : function(event, ui) {
+				$("#pwv").val(ui.value);
+				$("#amount_pwv").val(ui.value + " participant(s) with audio/video");
+				var camRes = $("#cam_res").val();
+				var numWithCam = $("#pwv").val();
+				var numWithoutCam = $("#pnv").val();
+				$("#sib").val(+camRes * +numWithCam);
+				$("#sob").val(+camRes * +numWithCam * (+numWithCam + +numWithoutCam - 1));
+				$("#uib").val(+camRes * +numWithCam);
+				$("#uob").val(+camRes);
+			}
+		});
+		$("#slider-num-without-cam").slider({
+			value : "0",
+			min : 0,
+			max : 100,
+			step : 1,
+			slide : function(event, ui) {
+				$("#pnv").val(ui.value);
+				$("#amount_pnv").val(ui.value + " participant(s) without audio/video");
+				var camRes = $("#cam_res").val();
+				var numWithCam = $("#pwv").val();
+				var numWithoutCam = $("#pnv").val();
+				$("#sib").val(+camRes * +numWithCam);
+				$("#sob").val(+camRes * +numWithCam * (+numWithCam + +numWithoutCam - 1));
+				$("#uib").val(+camRes * +numWithCam);
+				$("#uob").val(+camRes);
+			}
+		});
+		$("#cam_res").val('$' + $("#slider-cam-res").slider("value"));
+		$("#pwv").val('$' + $("#slider-num-with-cam").slider("value"));
+		$("#pnv").val('$' + $("#slider-num-without-cam").slider("value"));
 
-    $( function() {
-        var p = {
-              0: "None (Only audio ~44 kbit/s)",
-              1: "Cam resolution: 40x30 [4:3 (~83 kbit/s)]",
-              2: "Cam resolution: 80x60 [4:3 (~129 kbit/s)]",
-              3: "Cam resolution: 120x90 [4:3 (~196 kbit/s)]",
-              4: "Cam resolution: 160x120 [QQVGA 4:3 (~246 kbit/s)]",
-              5: "Cam resolution: 240x180 [4:3 (~251 kbit/s)]",
-              6: "Cam resolution: 320x240 [HVGA 4:3 (~252 kbit/s)]",
-              7: "Cam resolution: 480x360 [4:3  (~469 kbit/s)]",
-              8: "Cam resolution: 640x480 [4:3 (~904 kbit/s)]",
-              9: "Cam resolution: 1024x768 [XGA 4:3 (~2090 kbit/s)]",
-             10: "Cam resolution: 256x150 [16:9 (~252 kbit/s)]",
-             11: "Cam resolution: 432x240 [WQVGA 9:5 (~283 kbit/s)]",
-             12: "Cam resolution: 480x234 [pseudo 16:9 (~302 kbit/s)]",
-             13: "Cam resolution: 512x300 [16:9 (~411 kbit/s)]",
-             14: "Cam resolution: 640x360 [nHD 16:9 (~615 kbit/s)]",
-             15: "Cam resolution: 1024x600 [16:9 (~1624 kbit/s)] ",
-        };
-        var t = {
-
-             0: "44",
-             1: "83",
-             2: "129",
-             3: "196",
-             4: "246",
-             5: "251",
-             6: "252",
-             7: "469",
-             8: "904",
-             9: "2090",
-            10: "252",
-            11: "283",
-            12: "302",
-            13: "411",
-            14: "615",
-            15: "1624",
-        }
-
-        $("#slider-cam-res").slider({
-
-            value: "3",
-            min: 0,
-            max: 15,
-            step: 1,
-            slide: function(event, ui) {
-
-                $("#cam_res").val(t[ui.value]);
-                $("#amount_cam_res").val(p[ui.value]);
-                var camRes = $("#cam_res").val();
-                var numWithCam = $("#pwv").val();
-                var numWithoutCam = $("#pnv").val();
-                $("#sib").val(+camRes * +numWithCam);
-                $("#sob").val(+camRes * +numWithCam *( +numWithCam + +numWithoutCam - 1));
-                $("#uib").val(+camRes * +numWithCam);
-                $("#uob").val(+camRes );
-            }
-        });
-
-        $("#slider-num-with-cam").slider({
-            value: "2",
-            min: 0,
-            max: 100,
-            step: 1,
-            slide: function(event, ui) {
-                $("#pwv").val(ui.value);
-                $("#amount_pwv").val(ui.value + " participant(s) with audio/video");
-                var camRes = $("#cam_res").val();
-                var numWithCam = $("#pwv").val();
-                var numWithoutCam = $("#pnv").val();
-                $("#sib").val(+camRes * +numWithCam);
-                $("#sob").val(+camRes * +numWithCam *( +numWithCam + +numWithoutCam - 1));
-                $("#uib").val(+camRes * +numWithCam);
-                $("#uob").val(+camRes );
-            }
-        });
-        $("#slider-num-without-cam").slider({
-            value: "0",
-            min: 0,
-            max: 100,
-            step: 1,
-            slide: function(event, ui) {
-                $("#pnv").val(ui.value);
-                $("#amount_pnv").val(ui.value + " participant(s) without audio/video");
-                var camRes = $("#cam_res").val();
-                var numWithCam = $("#pwv").val();
-                var numWithoutCam = $("#pnv").val();
-                $("#sib").val(+camRes * +numWithCam);
-                $("#sob").val(+camRes * +numWithCam *( +numWithCam + +numWithoutCam - 1));
-                $("#uib").val(+camRes * +numWithCam);
-                $("#uob").val(+camRes );
-            }
-        });
-        $("#cam_res").val('$' + $("#slider-cam-res").slider("value"));
-        $("#pwv").val('$' + $("#slider-num-with-cam").slider("value"));
-        $("#pnv").val('$' + $("#slider-num-without-cam").slider("value"));
-
-        $("#cam_res").val("196");
-        $("#pwv").val("2");
-        $("#pnv").val("0");
-
-    }
-    );
-
+		$("#cam_res").val("196");
+		$("#pwv").val("2");
+		$("#pnv").val("0");
+	});
 });
--- openmeetings-server/src/site/resources/js/site.js
+++ openmeetings-server/src/site/resources/js/site.js
@@ -17,73 +17,10 @@
   under the License.
   
 */
-function getMenu(a) {
-	return a.parent().children('ul:first');
-}
-
 $(document).ready(function() {
-
-	console.log($(".example-image-link"));
-
-	$(".example-image-link").click(
-		function(e) {
-			e.preventDefault();
-			var addressValue = $(this).attr("href");
-			console.log("addressValue="+addressValue);
-
-			var overLay = '<div id="overlay"><div class="bg"></div><img src="'+addressValue+'"></div>';
-
-			var overlay = jQuery(overLay);
-			overlay.appendTo(document.body);
-
-			$("#overlay").click(
-				function(e) {
-					$("#overlay").remove();
-				}
-			);
-
-		}
-	);
-
-	$('ul.nav.nav-list > li a').each(function() {
-		var a = $(this);
-		var m = getMenu(a);
-		if (m.length) {
-			a.addClass('nav-header')
-			a.attr('data-toggle', 'collapse');
-			a.click(function(){
-				var a = $(this);
-				var v = getMenu(a).height() != 0;
-				a.children('i').removeClass(v ? 'icon-chevron-down' : 'icon-chevron-right').addClass(v ? 'icon-chevron-right' : 'icon-chevron-down');
-			})
-			m.attr('id', a.attr('href').substr(1));
-			if (!m.find('li.active').length) {
-				m.collapse('hide');
-				a.children('i').removeClass('icon-chevron-down').addClass('icon-chevron-right');
-			}
-		}
-	});
-	var c = $('#slider');
-	if (c.length) {
-		/* FIXME uncomment later
-		var items = $('.carousel-inner .item', c);
-		var ol = $('<ol>').addClass('carousel-indicators');
-		for (var i = 0; i < items.length; ++i) {
-			var li = $('<li>').attr('data-target', '#slider').attr('data-slide-to', i);
-			if ($(items[i]).hasClass('active')) {
-				li.addClass('active');
-			}
-			ol.append(li);
-		}
-		c.prepend(ol);
-		*/
-		$('.carousel-control.left', c).attr('data-slide', 'prev');
-		$('.carousel-control.right', c).attr('data-slide', 'next');
-		c.attr('data-ride', 'carousel').addClass('carousel slide');
-	}
 	// "New" markers
 	var topics = ["Call For Logo"];
 	for (var i = 0; i < topics.length; ++i) {
-		$('ul.nav.nav-list ul.nav.nav-list li:contains("' + topics[i] + '") a').append('&nbsp;&nbsp;<span class="label label-success">New</span>');
+		$('ul.nav li a[title="' + topics[i] + '"').append('&nbsp;&nbsp;<span class="label label-success">New</span>')
 	}
 })
