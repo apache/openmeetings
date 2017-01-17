0201a7c69c599f5001817d80a8ba5d932227b63d
Maxim Solodovnik <solomax@apache.org>
2017-01-17 17:36:44 +0000

    [OPENMEETINGS-1527] basic work on issue

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/common/tree/FileItemTree.java
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/common/tree/FileItemTree.java
@@ -19,132 +19,24 @@
 package org.apache.openmeetings.web.common.tree;
 
 import org.apache.openmeetings.db.entity.file.FileItem;
-import org.apache.openmeetings.db.entity.file.FileItem.Type;
-import org.apache.openmeetings.db.entity.record.Recording;
-import org.apache.openmeetings.db.entity.record.Recording.Status;
 import org.apache.wicket.Component;
-import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.markup.html.repeater.tree.DefaultNestedTree;
 import org.apache.wicket.extensions.markup.html.repeater.tree.ITreeProvider;
-import org.apache.wicket.extensions.markup.html.repeater.tree.content.Folder;
 import org.apache.wicket.markup.repeater.ReuseIfModelsEqualStrategy;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
 
 public class FileItemTree extends DefaultNestedTree<FileItem> {
 	private static final long serialVersionUID = 1L;
-	private final FileTreePanel treePanel;
-	private final static String CSS_CLASS_FILE = "file ";
+	final FileTreePanel treePanel;
 
 	public FileItemTree(String id, FileTreePanel treePanel, ITreeProvider<FileItem> tp) {
 		super(id, tp);
 		this.treePanel = treePanel;
 		setItemReuseStrategy(new ReuseIfModelsEqualStrategy());
 	}
-	
+
 	@Override
 	protected Component newContentComponent(String id, IModel<FileItem> node) {
-		return new Folder<FileItem>(id, this, node) {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			protected Component newLabelComponent(String id, final IModel<FileItem> lm) {
-				FileItem r = lm.getObject();
-				return Type.Folder == r.getType() || r.getId() == null ? new FolderPanel(id, lm, treePanel) : new FileItemPanel(id, lm, treePanel);
-			}
-			
-			@Override
-			protected boolean isSelected() {
-				FileItem f = getModelObject(), s = treePanel.getSelected();
-				return (s.getId() == null && s.getId() == f.getId() && s.getHash().equals(f.getHash()))
-						|| (s.getId() != null && s.getId().equals(f.getId()));
-			}
-			
-			@Override
-			protected boolean isClickable() {
-				return true;
-			}
-			
-			@Override
-			protected void onClick(AjaxRequestTarget target) {
-				FileItem r = getModelObject();
-				treePanel.setSelected(r, target);
-				if (Type.Folder == r.getType()) {
-					if (getState(r) == State.COLLAPSED) {
-						super.onClick(target);
-					}
-				} else {
-					treePanel.update(target, r);
-				}
-			}
-			
-			private String getItemStyle(FileItem f, boolean open) {
-				StringBuilder style = new StringBuilder("big om-icon ");
-				if (f.getId() == null) {
-					style.append(CSS_CLASS_FILE).append(f.getHash().indexOf("my") > -1 ? "my" : "public");
-				} else {
-					if (!f.exists()) {
-						style.append("broken ");
-					}
-					switch(f.getType()) {
-						case Folder:
-							style.append(CSS_CLASS_FILE).append(open ? "folder-open " : "folder ");
-							break;
-						case Image:
-							style.append(CSS_CLASS_FILE).append("image ");
-							break;
-						case PollChart:
-							style.append(CSS_CLASS_FILE).append("chart ");
-							break;
-						case WmlFile:
-							style.append(CSS_CLASS_FILE).append("wml ");
-							break;
-						case Video:
-						case Recording:
-						{
-							style.append("recording ");
-							if (f instanceof Recording) {
-								Status st = ((Recording)f).getStatus();
-								if (Status.RECORDING == st || Status.CONVERTING == st) {
-									style.append("processing");
-								}
-							}
-						}
-							break;
-						case Presentation:
-							style.append(CSS_CLASS_FILE).append("doc ");
-							break;
-						default:
-							break;
-					}
-				}
-				return style.toString();
-			}
-			
-			@Override
-			protected String getOtherStyleClass(FileItem r) {
-				return getItemStyle(r, false);
-			}
-			
-			@Override
-			protected String getOpenStyleClass() {
-				return getItemStyle(getModelObject(), true);
-			}
-			
-			@Override
-			protected String getClosedStyleClass() {
-				return getItemStyle(getModelObject(), false);
-			}
-			
-			@Override
-			protected String getSelectedStyleClass() {
-				return "ui-state-active";
-			}
-			
-			@Override
-			protected IModel<String> newLabelModel(IModel<FileItem> model) {
-				return Model.of(model.getObject().getName());
-			}
-		};
+		return new FileFolder(id, this, node, treePanel);
 	}
 }
