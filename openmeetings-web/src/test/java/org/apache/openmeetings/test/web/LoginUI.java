89ec91ad4117df01e1490569800ae66bbd01039d
Maxim Solodovnik <solomax@apache.org>
2017-01-22 16:48:27 +0000

    [OPENMEETINGS-816] loginUI wicket test is fixed

--- openmeetings-web/src/test/java/org/apache/openmeetings/test/web/LoginUI.java
+++ openmeetings-web/src/test/java/org/apache/openmeetings/test/web/LoginUI.java
@@ -18,35 +18,42 @@
  */
 package org.apache.openmeetings.test.web;
 
+import static org.junit.Assert.assertTrue;
+
 import org.apache.openmeetings.test.AbstractWicketTester;
+import org.apache.openmeetings.web.app.Application;
+import org.apache.openmeetings.web.app.WebSession;
 import org.apache.openmeetings.web.pages.MainPage;
 import org.apache.openmeetings.web.pages.auth.SignInPage;
 import org.apache.wicket.util.tester.FormTester;
+import org.junit.Test;
 
 public class LoginUI extends AbstractWicketTester {
-
-        //@Test Needs to much memory to start this test, and there is nothing useful here inside other then some basic
-        //proof of concept (basically the concept failed)
-	public void testLoginUi() {
-		
+	@Test
+	public void testValidLogin() {
 		tester.startPage(MainPage.class);
-		
 		tester.assertRenderedPage(SignInPage.class);
-		
+
 		FormTester formTester = tester.newFormTester("signin:signin");
 		formTester.setValue("login", username);
 		formTester.setValue("pass", userpass);
-		
-		//How to reference specific buttons in Wicket jQuery UI ?!
-		
-		formTester.submit();
-		
-		System.err.println("getLastRenderedPage: "+ tester.getLastRenderedPage().getMarkup().toString());
-		
-		//will fail
-		//tester.assertComponent("dashboard", DashboardPanel.class);
-		
-	}
-	
-}
+		formTester.submit("submit");
 
+		tester.assertNoErrorMessage();
+		tester.assertRenderedPage(MainPage.class);
+		WebSession ws = (WebSession)tester.getSession();
+		assertTrue("Login should be successful", ws.isSignedIn());
+	}
+
+	@Test
+	public void testEmptyLogin() {
+		tester.startPage(SignInPage.class);
+		tester.assertRenderedPage(SignInPage.class);
+
+		FormTester formTester = tester.newFormTester("signin:signin");
+		formTester.submit("submit");
+
+		tester.assertErrorMessages(String.format("'%s' is required.", Application.getString(114))
+				, String.format("'%s' is required.", Application.getString(115)));
+	}
+}
