e910279241b83065eb35ad79e1898543bef75241
Maxim Solodovnik <solomax@apache.org>
2017-01-23 15:38:29 +0000

    [OPENMEETINGS-816] first step to sign-up test using wicket tester

--- openmeetings-web/src/test/java/org/apache/openmeetings/test/web/LoginUI.java
+++ openmeetings-web/src/test/java/org/apache/openmeetings/test/web/LoginUI.java
@@ -18,15 +18,19 @@
  */
 package org.apache.openmeetings.test.web;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.openmeetings.test.AbstractWicketTester;
-import org.apache.openmeetings.web.app.Application;
 import org.apache.openmeetings.web.app.WebSession;
 import org.apache.openmeetings.web.pages.MainPage;
 import org.apache.openmeetings.web.pages.auth.SignInPage;
+import org.apache.wicket.feedback.ExactLevelFeedbackMessageFilter;
+import org.apache.wicket.feedback.FeedbackMessage;
 import org.apache.wicket.util.tester.FormTester;
 import org.junit.Test;
+
+import com.googlecode.wicket.jquery.ui.widget.dialog.ButtonAjaxBehavior;
 
 public class LoginUI extends AbstractWicketTester {
 	@Test
@@ -53,7 +57,21 @@ public class LoginUI extends AbstractWicketTester {
 		FormTester formTester = tester.newFormTester("signin:signin");
 		formTester.submit("submit");
 
-		tester.assertErrorMessages(String.format("'%s' is required.", Application.getString(114))
-				, String.format("'%s' is required.", Application.getString(115)));
+		assertEquals("There should be exactly 2 errors", 2,
+				tester.getFeedbackMessages(new ExactLevelFeedbackMessageFilter(FeedbackMessage.ERROR)).size());
+	}
+
+
+	@Test
+	public void testEmptyRegister() {
+		tester.startPage(SignInPage.class);
+		tester.assertRenderedPage(SignInPage.class);
+
+		ButtonAjaxBehavior b = getButtonBehavior("signin", "register");
+		tester.executeBehavior(b);
+		FormTester formTester = tester.newFormTester("register:form");
+		formTester.submit("submit");
+		assertEquals("There should be exactly 7 errors", 7,
+				tester.getFeedbackMessages(new ExactLevelFeedbackMessageFilter(FeedbackMessage.ERROR)).size());
 	}
 }
