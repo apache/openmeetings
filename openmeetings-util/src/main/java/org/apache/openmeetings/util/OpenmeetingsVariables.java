fb8e34bfe67a6822c05766a921a97142d8681630
Maxim Solodovnik <solomax666@gmail.com>
2017-05-17 00:54:04 +0700

    no jira: default value for Content-Security-Policy header is fixed

--- openmeetings-util/src/main/java/org/apache/openmeetings/util/OpenmeetingsVariables.java
+++ openmeetings-util/src/main/java/org/apache/openmeetings/util/OpenmeetingsVariables.java
@@ -57,7 +57,7 @@ public class OpenmeetingsVariables {
 	public static final String CONFIG_FLASH_VIDEO_FPS = "flash.video.fps";
 	public static final String CONFIG_HEADER_XFRAME = "header.x.frame.options";
 	public static final String HEADER_XFRAME_SAMEORIGIN = "SAMEORIGIN";
-	public static final String HEADER_CSP_SELF = "default-src 'self';";
+	public static final String HEADER_CSP_SELF = "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval';";
 	public static final String CONFIG_HEADER_CSP = "header.content.security.policy";
 	public static final String MENU_ROOMS_NAME = "Conference Rooms";
 	public static final int RECENT_ROOMS_COUNT = 5;
