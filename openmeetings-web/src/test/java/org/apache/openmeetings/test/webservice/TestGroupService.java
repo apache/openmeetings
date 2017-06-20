15ce5da19e724e55851c4a079db30ba5faea73a8
Maxim Solodovnik <solomax666@gmail.com>
2017-06-20 17:08:48 +0700

    no jira: unused HTTP methods are disabled

--- openmeetings-web/src/test/java/org/apache/openmeetings/test/webservice/TestGroupService.java
+++ openmeetings-web/src/test/java/org/apache/openmeetings/test/webservice/TestGroupService.java
@@ -21,23 +21,19 @@ package org.apache.openmeetings.test.webservice;
 import static org.apache.openmeetings.db.util.ApplicationHelper.getWicketTester;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
-import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 
 import org.apache.openmeetings.db.dto.basic.ServiceResult;
 import org.apache.openmeetings.db.dto.basic.ServiceResult.Type;
-import org.apache.openmeetings.db.dto.room.RoomOptionsDTO;
-import org.apache.openmeetings.db.dto.user.ExternalUserDTO;
 import org.apache.openmeetings.web.app.WebSession;
-import org.apache.wicket.util.string.StringValue;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.After;
 import org.junit.Test;
 
-public class TestUserService extends AbstractWebServiceTest {
+public class TestGroupService extends AbstractWebServiceTest {
 	protected WicketTester tester;
+	public final static String GROUP_SERVICE_URL = BASE_SERVICES_URL + "/group";
 
 	@Override
 	public void setUp() throws Exception {
@@ -47,43 +43,38 @@ public class TestUserService extends AbstractWebServiceTest {
 	}
 
 	@Test
-	public void invalidLoginTest() {
-		ServiceResult r = loginNoCheck("invalid-user", "bad pass");
-		assertNotNull("Valid ServiceResult should be returned", r);
-		assertEquals("Login should NOT be successful", Type.ERROR.name(), r.getType());
+	public void putTest() {
+		ServiceResult r = login();
+		Response resp = getClient(GROUP_SERVICE_URL)
+				.path("/")
+				.query("sid", r.getMessage()).put("");
+		assertEquals("Call should NOT be successful", Response.Status.METHOD_NOT_ALLOWED.getStatusCode(), resp.getStatus());
 	}
 
 	@Test
-	public void loginTest() {
+	public void addRemoveTest() {
 		ServiceResult r = login();
-		assertNotNull("Valid ServiceResult should be returned", r);
-	}
-
-	@Test
-	public void hashTest() {
-		ServiceResult r = login();
-		ExternalUserDTO user = new ExternalUserDTO();
-		user.setExternalId("1");
-		user.setExternalType("OmJunitTests");
-		user.setEmail("user1@junit.openmeetings.apache.org");
-		user.setFirstname("First Name 1");
-		user.setLastname("Last Name 1");
-		user.setLogin("login1");
-		RoomOptionsDTO options = new RoomOptionsDTO();
-		options.setRoomId(5L);
-		options.setModerator(true);
-		Response resp = getClient(USER_SERVICE_URL)
-				.path("/hash")
-				.query("sid", r.getMessage())
-				.form(new Form().param("user", user.toString()).param("options", options.toString()));
-		assertNotNull("Valid ServiceResult should be returned", resp);
-		assertEquals("Call should be successful", Response.Status.OK.getStatusCode(), resp.getStatus());
-		ServiceResult r1 = resp.readEntity(ServiceResult.class);
-		assertEquals("OM Call should be successful", r1.getType(), Type.SUCCESS.name());
-
-		WebSession ws = WebSession.get();
-		ws.checkHashes(StringValue.valueOf(r1.getMessage()), StringValue.valueOf(""));
-		assertTrue("Login via secure hash should be successful", ws.isSignedIn());
+		long groupId = -1;
+		{
+			Response resp = getClient(GROUP_SERVICE_URL)
+					.path("/")
+					.query("sid", r.getMessage()).query("name", "Test Group").post("");
+			assertNotNull("Valid ServiceResult should be returned", resp);
+			assertEquals("Call should be successful", Response.Status.OK.getStatusCode(), resp.getStatus());
+			ServiceResult r1 = resp.readEntity(ServiceResult.class);
+			assertEquals("OM Call should be successful", r1.getType(), Type.SUCCESS.name());
+			groupId = r1.getCode();
+		}
+		//delete group created
+		{
+			Response resp = getClient(GROUP_SERVICE_URL)
+					.path("/" + groupId)
+					.query("sid", r.getMessage()).delete();
+			assertNotNull("Valid ServiceResult should be returned", resp);
+			assertEquals("Call should be successful", Response.Status.OK.getStatusCode(), resp.getStatus());
+			ServiceResult r1 = resp.readEntity(ServiceResult.class);
+			assertEquals("OM Call should be successful", r1.getType(), Type.SUCCESS.name());
+		}
 	}
 
 	@After
