1ec5e2840b16f7ceb111780d0c27564382a68989
Maxim Solodovnik <solomax666@gmail.com>
2017-06-21 10:42:39 +0700

    [OPENMEETINGS-1655] whiteboard can be hidden

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/room/RoomPanel.java
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/room/RoomPanel.java
@@ -308,6 +308,7 @@ public class RoomPanel extends BasePanel {
 		}
 		if (room.isVisible()) {
 			add(new NicknameDialog("nickname", this));
+			wbArea.setVisible(!r.isHidden(RoomElement.Whiteboard));
 		} else {
 			add(new WebMarkupContainer("nickname").setVisible(false));
 		}
