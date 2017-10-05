50ec59c5c0c6097e35f6ba72c83ec35a79bc71dc
Maxim Solodovnik <solomax666@gmail.com>
2017-10-05 16:57:34 +0700

    [OPENMEETINGS-1714] coverage is improved

---
+++ openmeetings-web/src/test/java/org/apache/openmeetings/webservice/TestInfoService.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License") +  you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.openmeetings.webservice;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.openmeetings.db.dto.basic.Info;
+import org.junit.Test;
+
+public class TestInfoService extends AbstractWebServiceTest {
+	public static final String INFO_SERVICE_URL = BASE_SERVICES_URL + "/info";
+
+	@Test
+	public void infoTest() {
+		Info info = getClient(INFO_SERVICE_URL).path("/version").get(Info.class);
+		assertNotNull("Valid info should be returned", info);
+		assertNotNull("Valid BuildDate should be returned", info.getBuildDate());
+		assertNotNull("Valid Revision should be returned", info.getRevision());
+		assertNotNull("Valid Version should be returned", info.getVersion());
+	}
+}
