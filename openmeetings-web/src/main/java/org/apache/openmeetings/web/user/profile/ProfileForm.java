f13d98d3c8a2d15fc2365df2b5b53246a122dc1f
Maxim Solodovnik <solomax666@gmail.com>
2018-07-26 18:13:45 +0700

    [OPENMEETINGS-1898] user details can be changed without password for oauth user

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/user/profile/ProfileForm.java
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/user/profile/ProfileForm.java
@@ -53,6 +53,7 @@ public class ProfileForm extends Form<User> {
 	private final PasswordTextField passwd = new PasswordTextField("passwd", new Model<String>());
 	private final GeneralUserForm userForm;
 	private final ChangePasswordDialog chPwdDlg;
+	private boolean checkPassword;
 	private FormActionsPanel<User> actions;
 
 	@SpringBean
@@ -63,12 +64,13 @@ public class ProfileForm extends Form<User> {
 		setModel(new CompoundPropertyModel<>(userDao.get(getUserId())));
 		userForm = new GeneralUserForm("general", getModel(), false);
 		this.chPwdDlg = chPwdDlg;
+		this.checkPassword = User.Type.oauth != getModelObject().getType();
 	}
 
 	@Override
 	protected void onInitialize() {
 		super.onInitialize();
-		add(passwd.setLabel(Model.of(getString("current.password"))).setRequired(true));
+		add(passwd.setLabel(Model.of(getString("current.password"))).setRequired(true).setVisible(checkPassword));
 
 		add(actions = new FormActionsPanel<User>("buttons", this) {
 			private static final long serialVersionUID = 1L;
@@ -114,7 +116,7 @@ public class ProfileForm extends Form<User> {
 			protected void onEvent(AjaxRequestTarget target) {
 				chPwdDlg.open(target);
 			}
-		}));
+		}).setVisible(checkPassword));
 		add(userForm);
 		add(new UploadableProfileImagePanel("img", getUserId()));
 		add(new ComunityUserForm("comunity", getModel()));
@@ -132,14 +134,16 @@ public class ProfileForm extends Form<User> {
 
 	@Override
 	protected void onValidate() {
-		String p = passwd.getConvertedInput();
-		if (!Strings.isEmpty(p) && !userDao.verifyPassword(getModelObject().getId(), p)) {
-			error(getString("231"));
-			// add random timeout
-			try {
-				Thread.sleep(6 + (long)(10 * Math.random() * 1000));
-			} catch (InterruptedException e) {
-				log.error("Unexpected exception while sleeping", e);
+		if (checkPassword) {
+			String p = passwd.getConvertedInput();
+			if (!Strings.isEmpty(p) && !userDao.verifyPassword(getModelObject().getId(), p)) {
+				error(getString("231"));
+				// add random timeout
+				try {
+					Thread.sleep(6 + (long)(10 * Math.random() * 1000));
+				} catch (InterruptedException e) {
+					log.error("Unexpected exception while sleeping", e);
+				}
 			}
 		}
 		super.onValidate();
