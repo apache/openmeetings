f36ae90fdc721ec7fc33710f4aab5ce9e361aa47
Maxim Solodovnik <solomax666@gmail.com>
2018-10-02 17:10:22 +0700

    [OPENMEETINGS-1944] drag files and video play are fixed

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/common/tree/filetree.js
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/common/tree/filetree.js
@@ -2,9 +2,9 @@
 function dragHelper() {
 	let s = $(this);
 	if (s.hasClass('ui-state-active')) {
-		s = $('.ui-state-active .ui-draggable.ui-draggable-handle');
+		s = $('.file.ui-state-active.ui-draggable.ui-draggable-handle, .recording.ui-state-active.ui-draggable.ui-draggable-handle');
 	}
-	const c = $('<div/>').attr('id', 'draggingContainer').width(80).height(36)
+	const c = $('<div/>').attr('id', 'draggingContainer').addClass('drag-container').width(80).height(36)
 		, h = $('<div class="ui-corner-all ui-widget-header"/>').append(s.clone()).width(s.width());
 	return c.append(h);
 }
