172fff987104854ebaf0cec09217b014b747a5bf
Maxim Solodovnik <solomax666@gmail.com>
2020-02-18 00:32:32 +0700

    no jira: project is partially converted to use java11 modules

---
+++ openmeetings-db/src/main/java/module-info.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License") +  you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+module apache.openmeetings.db {
+	exports org.apache.openmeetings;
+	exports org.apache.openmeetings.db.dao;
+	exports org.apache.openmeetings.db.dao.basic;
+	exports org.apache.openmeetings.db.dao.calendar;
+	exports org.apache.openmeetings.db.dao.file;
+	exports org.apache.openmeetings.db.dao.label;
+	exports org.apache.openmeetings.db.dao.log;
+	exports org.apache.openmeetings.db.dao.record;
+	exports org.apache.openmeetings.db.dao.room;
+	exports org.apache.openmeetings.db.dao.server;
+	exports org.apache.openmeetings.db.dao.user;
+	exports org.apache.openmeetings.db.dto.basic;
+	exports org.apache.openmeetings.db.dto.calendar;
+	exports org.apache.openmeetings.db.dto.file;
+	exports org.apache.openmeetings.db.dto.record;
+	exports org.apache.openmeetings.db.dto.room;
+	exports org.apache.openmeetings.db.dto.user;
+	exports org.apache.openmeetings.db.entity;
+	exports org.apache.openmeetings.db.entity.basic;
+	exports org.apache.openmeetings.db.entity.calendar;
+	exports org.apache.openmeetings.db.entity.file;
+	exports org.apache.openmeetings.db.entity.label;
+	exports org.apache.openmeetings.db.entity.log;
+	exports org.apache.openmeetings.db.entity.record;
+	exports org.apache.openmeetings.db.entity.room;
+	exports org.apache.openmeetings.db.entity.server;
+	exports org.apache.openmeetings.db.entity.user;
+	exports org.apache.openmeetings.db.manager;
+	exports org.apache.openmeetings.db.util;
+	exports org.apache.openmeetings.db.util.ws;
+
+	requires transitive apache.openmeetings.util;
+	requires transitive org.apache.wicket.request;
+	requires transitive org.apache.wicket.websocket.core;
+	requires transitive spring.beans;
+	requires transitive simple.xml;
+	requires transitive spring.web;
+	requires transitive spring.core;
+
+	requires spring.tx;
+	requires spring.context;
+	requires java.persistence;
+	requires openjpa;
+	requires asterisk.java;
+	requires java.xml.bind;
+	requires org.apache.commons.text;
+}
---
+++ openmeetings-util/src/main/java/module-info.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License") +  you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+module apache.openmeetings.util {
+	exports org.apache.openmeetings.util;
+	exports org.apache.openmeetings.util.crypt;
+	exports org.apache.openmeetings.util.mail;
+	exports org.apache.openmeetings.util.process;
+	exports org.apache.openmeetings.util.ws;
+
+	requires transitive javax.servlet.api;
+
+	requires transitive org.apache.wicket.util;
+	requires transitive org.apache.wicket.core;
+	requires transitive org.apache.wicket.extensions;
+
+	requires transitive org.apache.commons.codec;
+	requires transitive org.apache.commons.lang3;
+	requires transitive org.apache.tika.core;
+
+	requires transitive com.github.openjson;
+	requires transitive org.slf4j;
+	requires transitive logback.classic;
+	requires transitive logback.core;
+	requires transitive dom4j;
+	requires transitive jcip.annotations;
+	requires transitive org.bouncycastle.provider;
+	requires transitive java.activation;
+	requires transitive org.mnode.ical4j.core;
+}
