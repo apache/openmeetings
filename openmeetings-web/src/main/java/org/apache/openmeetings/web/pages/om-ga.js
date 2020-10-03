6b379b7c8ab5c81c91b1dfbd0a8957d41b9c5893
Maxim Solodovnik <solomax666@gmail.com>
2020-10-04 00:49:26 +0700

    [OPENMEETINGS-2472] JS error is fixed

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/pages/om-ga.js
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/pages/om-ga.js
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License") http://www.apache.org/licenses/LICENSE-2.0 */
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+	dataLayer.push(arguments);
+}
 function initGA(code) {
-	window.dataLayer = window.dataLayer || [];
-	function gtag() {
-		dataLayer.push(arguments);
-	}
 	gtag('js', new Date());
 
 	gtag('config', code);
