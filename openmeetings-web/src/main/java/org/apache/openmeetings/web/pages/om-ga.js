640c50916a7b5c3da9c775597e0309887b3bc42d
Maxim Solodovnik <solomax666@gmail.com>
2020-10-04 12:57:53 +0700

    [OPENMEETINGS-2472] gtag should work

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/pages/om-ga.js
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/pages/om-ga.js
@@ -1,12 +1,25 @@
 /* Licensed under the Apache License, Version 2.0 (the "License") http://www.apache.org/licenses/LICENSE-2.0 */
 function initGA(code, hash) {
 	window.dataLayer = window.dataLayer || [];
-	const gtag = () => dataLayer.push(arguments)
-		, newPage = () => gtag('config', code, {'page_path': location.pathname + location.hash});
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	function newPage() {
+		const page = location.pathname + (location.search.indexOf('app=') > -1 ? location.search : location.hash);
+		gtag('config', code, {'page_path': page});
+	}
 	gtag('js', new Date());
 
+	gtag('config', code, {
+		anonymize_ip: true
+		, send_page_view: false
+	});
 	newPage();
 	if (hash) {
-		$(window).off('hashchange').on('hashchange', newPage);
+		$(window).off('hashchange').on('hashchange', () => {
+			if (location.hash.indexOf('/') > -1) {
+				newPage();
+			}
+		});
 	}
 }
