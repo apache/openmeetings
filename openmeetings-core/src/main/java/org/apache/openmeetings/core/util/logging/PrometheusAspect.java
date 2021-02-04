4e97c82b367a3400b29e8322107de6f8f3b9bd52
Sebastian Wagner <sebawagner@apache.org>
2021-02-04 16:05:58 +1300

    OPENMEETINGS-2567 Fix Timer and settings for it.

--- openmeetings-core/src/main/java/org/apache/openmeetings/core/util/logging/PrometheusAspect.java
+++ openmeetings-core/src/main/java/org/apache/openmeetings/core/util/logging/PrometheusAspect.java
@@ -19,32 +19,46 @@
  */
 package org.apache.openmeetings.core.util.logging;
 
+import java.util.HashMap;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import io.prometheus.client.Summary;
 
 @Aspect
 @Component
 public class PrometheusAspect {
 
-	private static final Logger log = LoggerFactory.getLogger(PrometheusAspect.class);
+	private final HashMap<String, Summary> summaries = new HashMap<String, Summary>();
+
+	private Summary getSummary(String className, String methodName) {
+		String key = className + "_" + methodName;
+		Summary sum = summaries.get(key);
+		if (sum != null) {
+			return sum;
+		}
+		sum = Summary.build() //
+			.name(key) //
+			.help(key) //
+			.register();
+		summaries.put(key, sum);
+		return sum;
+	}
 
 	@Around("@annotation(Timed)")
 	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
-		final long start = System.currentTimeMillis();
-
-		final Object proceed = joinPoint.proceed();
-
-		final long executionTime = System.currentTimeMillis() - start;
-
-		log.warn(joinPoint.getSignature() + " ################################# executed in " + executionTime + "ms");
-		System.out.println(
-				joinPoint.getSignature() + " ################################# executed in " + executionTime + "ms");
-
-		return proceed;
+		String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+		String methodName = joinPoint.getSignature().getName();
+		Summary sum = getSummary(className, methodName);
+		Summary.Timer requestTimer = sum.startTimer();
+		try {
+			return joinPoint.proceed();
+		} finally {
+			requestTimer.observeDuration();
+		}
 	}
 
 }
