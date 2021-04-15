8c85ff4ba15dae364e64e33a0bcc9673d6d35d16
Maxim Solodovnik <solomax666@gmail.com>
2021-04-15 20:07:16 +0700

    [OPENMEETINGS-2612] field limits are added

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/admin/configurations/ConfigForm.java
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/admin/configurations/ConfigForm.java
@@ -18,6 +18,8 @@
  */
 package org.apache.openmeetings.web.admin.configurations;
 
+import static org.apache.wicket.validation.validator.StringValidator.maximumLength;
+
 import java.util.List;
 
 import org.apache.openmeetings.db.dao.basic.ConfigurationDao;
@@ -153,7 +155,8 @@ public class ConfigForm extends AdminBaseForm<Configuration> {
 					validatable.error(new ValidationError(getString("error.cfg.exist")));
 				}
 			}
-		}));
+		}).add(maximumLength(255)));
+		valueS.add(maximumLength(255));
 		stringBox.add(valueS.setLabel(new ResourceModel("271"))).setOutputMarkupId(true).setOutputMarkupPlaceholderTag(true);
 		numberBox.add(valueN.setLabel(new ResourceModel("271"))).setOutputMarkupId(true).setOutputMarkupPlaceholderTag(true);
 		booleanBox.add(valueB.setLabel(new ResourceModel("271"))).setOutputMarkupId(true).setOutputMarkupPlaceholderTag(true);
