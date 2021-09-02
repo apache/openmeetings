2f802e9d7d1b0df69e79dacb2eb18958585b7465
Maxim Solodovnik <solomax666@gmail.com>
2021-09-02 17:55:36 +0700

    [OPENMEETINGS-2652] test is fixed

--- openmeetings-web/src/test/java/org/apache/openmeetings/web/TestCalendar.java
+++ openmeetings-web/src/test/java/org/apache/openmeetings/web/TestCalendar.java
@@ -39,7 +39,7 @@ import com.googlecode.wicket.jquery.ui.calendar.CalendarView;
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.Navbar;
 
 class TestCalendar extends AbstractWicketTester {
-	private static final String PATH_APPOINTMENT_DLG = String.format("%s:appointment", PATH_CHILD);
+	private static final String PATH_APPOINTMENT_DLG = String.format("%s:calendarAppointment", PATH_CHILD);
 	private static final String PATH_APPOINTMENT_DLG_FRM = String.format("%s:appForm", PATH_APPOINTMENT_DLG);
 
 	@Test
