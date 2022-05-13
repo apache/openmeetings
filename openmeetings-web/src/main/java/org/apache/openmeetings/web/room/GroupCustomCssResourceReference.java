d7b916b0c835cbe083823c0ff693d5e079df3dbd
Maxim Solodovnik <solomax666@gmail.com>
2022-05-13 11:14:55 +0700

    [OPENMEETINGS-2703] couple more Sonar issues

--- openmeetings-web/src/main/java/org/apache/openmeetings/web/room/GroupCustomCssResourceReference.java
+++ openmeetings-web/src/main/java/org/apache/openmeetings/web/room/GroupCustomCssResourceReference.java
@@ -23,6 +23,8 @@ import static org.apache.openmeetings.util.OmFileHelper.getGroupCss;
 import java.io.File;
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.resource.FileSystemResource;
@@ -48,14 +50,16 @@ public class GroupCustomCssResourceReference extends FileSystemResourceReference
 	public IResource getResource() {
 		return new FileSystemResource() {
 			private static final long serialVersionUID = 1L;
+			private static final String MIME = "text/css";
 
 			@Override
 			protected String getMimeType() throws IOException {
-				return "text/css";
+				return MIME;
 			}
 
 			@Override
 			protected ResourceResponse newResourceResponse(Attributes attr) {
+				ResourceResponse rr;
 				PageParameters params = attr.getParameters();
 				StringValue idStr = params.get("id");
 				Long id = null;
@@ -65,14 +69,16 @@ public class GroupCustomCssResourceReference extends FileSystemResourceReference
 					//no-op expected
 				}
 				File file = getGroupCss(id, true);
-				if (file != null) {
-					ResourceResponse rr = createResourceResponse(attr, file.toPath());
-					rr.setFileName(file.getName());
-					return rr;
-				} else {
+				if (file == null) {
 					log.debug("Custom CSS was not found");
-					return null;
+					rr = new ResourceResponse()
+							.setContentType(MIME)
+							.setStatusCode(HttpServletResponse.SC_NO_CONTENT);
+				} else {
+					rr = createResourceResponse(attr, file.toPath());
+					rr.setFileName(file.getName());
 				}
+				return rr;
 			}
 		};
 	}
