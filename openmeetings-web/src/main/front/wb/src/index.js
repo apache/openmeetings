2925e9f7c6b9dcf49ba6ab4d7062cb595d2b9672
Maxim Solodovnik <solomax666@gmail.com>
2022-06-10 12:31:13 +0700

    [OPENMEETINGS-2732] tomcat, jettison, postgresql, mssql-driver, oracle-driver, MathJax

--- openmeetings-web/src/main/front/wb/src/index.js
+++ openmeetings-web/src/main/front/wb/src/index.js
@@ -1,12 +1,5 @@
 /* Licensed under the Apache License, Version 2.0 (the "License") http://www.apache.org/licenses/LICENSE-2.0 */
 
-// HACK!!!!! to avoid eval to determine MathJax version
-const fs = require('fs')
-	, pkg_src = fs.readFileSync(__dirname + '/../node_modules/mathjax-full/package.json', 'utf8');
-
-window.PACKAGE_VERSION = JSON.parse(pkg_src).version;
-// END HACK
-
 Object.assign(window, {
 	InterviewWbArea: require('./interview-area')
 	, DrawWbArea: require('./wb-area')
